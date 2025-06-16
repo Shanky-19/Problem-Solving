ad4a0f3135938b2ebfd0c17fd3b9960240b76a69
Shanky-19 <banaitsanket4@gmail.com>
2025-06-16 19:12:07 +0530

    Time: 1 ms (57.39%) | Memory: 42.7 MB (8.09%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -1,5 +1,7 @@
 class Solution {
     public int maximumDifference(int[] arr) {
+
+        /*
         int n = arr.length;
         int ans = -1;
         for(int i=0;i<n;i++) {
@@ -7,6 +9,20 @@ class Solution {
                 if(arr[i] < arr[j]) {
                     ans = Math.max(ans, arr[j]-arr[i]);
                 }
+            }
+        }
+        return ans;
+        */
+
+        // Using Stack
+        LinkedList<Integer> stack = new LinkedList<>();
+        int ans = -1;
+        stack.addFirst(arr[0]);
+        for(int i=1;i<arr.length;i++) {
+            if(arr[i] <= stack.getFirst()) {
+                stack.addFirst(arr[i]);
+            } else{
+                ans = Math.max(ans, arr[i]-stack.getFirst());
             }
         }
         return ans;
