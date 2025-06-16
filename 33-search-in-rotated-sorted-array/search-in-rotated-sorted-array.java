864d3de05170a6068534131cc29e875e4adc879f
Shanky-19 <banaitsanket4@gmail.com>
2025-06-16 21:33:12 +0530

    Time: 3 ms (3.27%) | Memory: 42.5 MB (11.75%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,51 @@
+class Solution {
+    
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+
+        if(nums.length == 1) {
+            return (nums[0] == target) ? 0 : -1;
+        }
+        
+        // smallest element idx = pivot index (where rotation happened)
+        int pivotIdx = findPivot(nums, low, high);
+        System.out.println(pivotIdx);
+        int idx1 = binarySearch(nums, low, pivotIdx, target);
+        if(nums[idx1] == target) {
+            return idx1;
+        }
+        int idx2 = binarySearch(nums, pivotIdx, high, target);
+        if(nums[idx2] == target) {
+            return idx2;
+        }
+        return -1;
+    }
+
+    private int findPivot(int[] nums, int low, int high) {
+        while(low < high) {
+            int mid = low + (high - low)/2;
+            if(nums[mid] > nums[high]) {
+                low = mid+1;
+            }else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    private int binarySearch(int[] nums, int low, int high, int target) {
+        while(low < high) {
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }else if(nums[mid] < target) {
+                low = mid+1;
+            }else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
