6bbb3ae3a2ff3bf54cf49fad9aa073241d7e64ec
Shanky-19 <banaitsanket4@gmail.com>
2025-06-18 22:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (72.45%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         // brute force
+        /*
         int num = 1;
         int i = 0;
         while(i< arr.length && k > 0) {
@@ -17,6 +18,25 @@ class Solution {
             num++;
         }
         return num-1;
+        */
+
+
+        // Binary Search
+
+        int n = arr.length;
+        int l = 0;
+        int r = n-1;
+        int mid = -1;
+        while(l <= r) {
+            mid = l+ (r-l)/2;
+            int number_of_missing_till_mid = arr[mid] - (mid+1);
+            if(number_of_missing_till_mid < k) {
+                l = mid+1;
+            }else {
+                r = mid-1;
+            }
+        }
+        return l+k;
         
         
     }
