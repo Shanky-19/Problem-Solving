9e33a10b1d2743c5cfb9b75e46999db851093929
Shanky-19 <banaitsanket4@gmail.com>
2025-06-22 18:30:05 +0530

    Time: 243 ms (60.77%) | Memory: 62 MB (68.13%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.java
+++ 3885-count-special-triplets/count-special-triplets.java
@@ -26,22 +26,22 @@ class Solution {
         */
 
         // optimized
-        final int MOD = 1_000_000_007;
-        Map<Integer, Integer> left = new HashMap<>();
-        Map<Integer, Integer> right = new HashMap<>();
+        final int max = 1000000007;
+        Map<Integer, Integer> leftMap = new HashMap<>();
+        Map<Integer, Integer> rightMap = new HashMap<>();
 
-        for (int num : nums)
-            right.put(num, right.getOrDefault(num, 0) + 1);
-
-        long res = 0;
         for (int num : nums) {
-            right.put(num, right.get(num) - 1);
-            res += 1L * left.getOrDefault(num * 2, 0) * right.getOrDefault(num * 2, 0);
-            res %= MOD;
-            left.put(num, left.getOrDefault(num, 0) + 1);
+            rightMap.put(num, rightMap.getOrDefault(num, 0) + 1);
         }
-
-        return (int) res;
-
+        long ans = 0;
+        for (int num : nums) {
+            rightMap.put(num, rightMap.get(num) - 1);
+            ans += 1L * 
+            leftMap.getOrDefault(num * 2, 0) 
+            * rightMap.getOrDefault(num * 2, 0);
+            ans %= max;
+            leftMap.put(num, leftMap.getOrDefault(num, 0) + 1);
+        }
+        return (int) ans;
     }
 }
