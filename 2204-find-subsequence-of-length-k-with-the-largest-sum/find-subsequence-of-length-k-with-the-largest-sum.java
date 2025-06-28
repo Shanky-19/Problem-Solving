9cb7aaae67fd06b9846501d36274e0c13ebe717e
Shanky-19 <banaitsanket4@gmail.com>
2025-06-28 20:14:06 +0530

    Time: 7 ms (83.20%) | Memory: 45.1 MB (8.92%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,22 +1,41 @@
 class Solution {
+
+    class Pair implements Comparable<Pair>{
+        int idx;
+        int val;
+
+        public Pair(int idx, int val) {
+            this.idx = idx;
+            this.val = val;
+        }
+
+        public int compareTo(Pair p) {
+            return this.val - p.val;
+        }
+    }
+
     public int[] maxSubsequence(int[] nums, int k) {
         int n = nums.length;
-        PriorityQueue<int[]> heap = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        for (int i = 0; i < n; i++) {
-            heap.add(new int[] {i, nums[i]});
-            if (heap.size() > k) {
-                heap.poll();
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>();
+        for(int i=0;i<n;i++) {
+            Pair p = new Pair(i, nums[i]);
+            minHeap.add(p);
+            if(minHeap.size() > k) {
+                minHeap.poll();
             }
         }
 
-        List<int[]> list = new ArrayList<>(heap);
-        list.sort(Comparator.comparingInt(a -> a[0]));
+        // for maintaining order as it is subsequence
+        List<Pair> list = new ArrayList<>(minHeap);
+        Collections.sort(list, (a, b) -> {
+            return a.idx - b.idx;
+        });
 
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = list.get(i)[1];
+        int[] ans = new int[list.size()];
+        for(int i=0;i<list.size();i++) {
+            ans[i] = list.get(i).val;
         }
+        return ans;
 
-        return result;
     }
 }
