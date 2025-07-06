8746e17e9303718cddfddb4aa05c33998d215a3e
Shanky-19 <banaitsanket4@gmail.com>
2025-07-06 20:10:43 +0530

    Time: 177 ms (13.68%) | Memory: 100.9 MB (5.26%) - LeetSync

---
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.java
@@ -0,0 +1,47 @@
+class FindSumPairs {
+    int[] nums1;
+    int[] nums2;
+    Map<Integer, Integer> nums2IdxVsValue;
+    Map<Integer, Integer> nums2ValueVsNumberOfIdx;
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        this.nums2IdxVsValue = new HashMap<>();
+        this.nums2ValueVsNumberOfIdx = new HashMap<>();
+        for(int idx=0;idx<nums2.length;idx++) {
+            nums2IdxVsValue.put(idx, nums2[idx]);
+            nums2ValueVsNumberOfIdx.put(nums2[idx], nums2ValueVsNumberOfIdx.getOrDefault(nums2[idx], 0)+1);
+        }
+    }
+    
+    public void add(int index, int val) {
+        int currentValue = nums2[index];
+        nums2[index] += val;
+        nums2IdxVsValue.put(index, nums2[index]);
+        if(nums2ValueVsNumberOfIdx.containsKey(currentValue)) {
+            nums2ValueVsNumberOfIdx.put(currentValue, nums2ValueVsNumberOfIdx.get(currentValue) - 1);
+            if(nums2ValueVsNumberOfIdx.get(currentValue) <= 0) {
+                nums2ValueVsNumberOfIdx.remove(currentValue);
+            }
+        } 
+        nums2ValueVsNumberOfIdx.put(nums2[index], nums2ValueVsNumberOfIdx.getOrDefault(nums2[index], 0)+1);
+    }
+    
+    public int count(int tot) {
+        int ans = 0;
+        for(int i=0;i<nums1.length;i++) {
+            int remainingValue = tot - nums1[i];
+            if(nums2ValueVsNumberOfIdx.containsKey(remainingValue)) {
+                ans += nums2ValueVsNumberOfIdx.get(remainingValue);
+            }
+        }
+        return ans;
+    }
+}
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs obj = new FindSumPairs(nums1, nums2);
+ * obj.add(index,val);
+ * int param_2 = obj.count(tot);
+ */
