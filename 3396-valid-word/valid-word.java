cbca82a4604c6dda7785d474df1c26fee5ff9d98
Shanky-19 <banaitsanket4@gmail.com>
2025-07-15 20:00:51 +0530

    Time: 2 ms (30.46%) | Memory: 42.3 MB (34.66%) - LeetSync

---
+++ 3396-valid-word/valid-word.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String word) {
+        word = word.toLowerCase();
+        if(word.length() < 3) {
+            return false;
+        }
+        int vowels = 0;
+        int consonants = 0;
+        for(char ch : word.toCharArray()) {
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            }else if(ch - 'a' >= 0 && ch - 'a' < 26) {
+                consonants++;
+            }else if(!(ch - '0' >=0 && ch - '0' <= 9)) {
+                return false;
+            }
+        }
+        if(vowels > 0 && consonants > 0) {
+            return true;
+        }
+        return false;
+    }
+}
