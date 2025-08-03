bae34e354522537602cb8255d027f76b99c197c7
Shanky-19 <banaitsanket4@gmail.com>
2025-08-04 00:18:16 +0530

    Time: 171 ms (11.39%) | Memory: 81.6 MB (98.73%) - LeetSync

---
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.java
@@ -0,0 +1,76 @@
+class Solution {
+
+    // Helper method to find the lower bound index
+    private int lowerBound(List<Integer> arr, int target) {
+        int left = 0, right = arr.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    // Helper method to find the upper bound index
+    private int upperBound(List<Integer> arr, int target) {
+        int left = 0, right = arr.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr.get(mid) <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+
+    public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        int n = fruits.length;
+        List<Integer> prefixSum = new ArrayList<>();
+        List<Integer> positions = new ArrayList<>();
+
+        for(int i=0;i<n;i++) {
+            positions.add(fruits[i][0]);
+
+            if(i > 0) {
+                prefixSum.add(fruits[i][1] + prefixSum.get(i-1));
+            } else {
+                prefixSum.add(fruits[i][1]);
+            }
+        }
+
+        int maxFruits = 0;
+        // case-1 : left side d distance chalo
+        for(int d=0; d<=k/2; d++) {
+            int i = startPos - d;
+            int j = startPos + k - 2*d;
+            int left = lowerBound(positions, i);
+            int right = upperBound(positions, j)-1;
+            
+            if (left <= right) {
+                int result = prefixSum.get(right) - (left > 0 ? prefixSum.get(left - 1) : 0);
+                maxFruits = Math.max(maxFruits, result);                
+            }
+            
+        }
+
+        // case-2 : right side d distance chalo
+        for(int d=0; d<=k/2; d++) {
+            int i = startPos - (k-2*d);
+            int j = startPos + d;
+            int left = lowerBound(positions, i);
+            int right = upperBound(positions, j)-1;
+            
+            if (left <= right) {
+                int result = prefixSum.get(right) - (left > 0 ? prefixSum.get(left - 1) : 0);
+                maxFruits = Math.max(maxFruits, result);                
+            }
+        }
+        return maxFruits;
+    }
+}
