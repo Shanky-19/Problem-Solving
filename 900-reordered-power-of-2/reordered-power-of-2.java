e2a67d31870d80432f3725192a66306fdc32ee85
Shanky-19 <banaitsanket4@gmail.com>
2025-08-10 09:10:07 +0530

    Time: 6 ms (9.47%) | Memory: 41.4 MB (14.21%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        List<Long> list = new ArrayList<>();
+        long val = 1L;
+        long max = (long) Math.pow(10,9);
+        while (val <= max) {
+            list.add(val);
+            val *= 2;
+        }
+        
+        // System.out.println(list);
+        Map<Integer, Integer> map1 = new HashMap<>();
+        while(n > 0) {
+            int lastDigit = n%10;
+            n = n/10;
+            map1.put(lastDigit, map1.getOrDefault(lastDigit, 0) + 1);
+        }
+        // System.out.println(map1);
+        for(long value : list) {
+            Map<Integer, Integer> map2 = new HashMap<>();
+            while(value > 0) {
+                int lastDigit = (int) value%10;
+                value = value/10;
+                map2.put(lastDigit, map2.getOrDefault(lastDigit, 0) + 1);
+            }
+            // System.out.println(map2);
+            if(map1.equals(map2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
