6296922f5e14d5b2a18002510bf91d9a0b60155f
Shanky-19 <banaitsanket4@gmail.com>
2025-08-16 17:06:45 +0530

    Time: 319 ms (5.00%) | Memory: 78.2 MB (43.44%) - LeetSync

---
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+        // brute force
+        /*
+        int n = people.length;
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++) {
+            int pep = people[i];
+            int count = 0;
+            for(int[] flower : flowers) {
+                if(pep >= flower[0] && pep <= flower[1]) {
+                    count++;
+                }
+            } 
+            ans[i] = count;
+        }
+        return ans;
+        */
+
+
+        // optimized
+        int n = people.length;
+        int m = flowers.length;
+        int[] ans = new int[n];
+        int[] start = new int[m];
+        int[] end = new int[m];
+        for(int i=0;i<m;i++) {
+            int[] flower = flowers[i];
+            start[i] = flower[0];
+            end[i] = flower[1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        for(int i=0;i<n;i++) {
+            int bloomed = 0;
+            int low = 0;
+            int high = m-1;
+            int personsTime = people[i];
+
+            while (low <= high) {
+                int mid = low + (high - low)/2;
+                if(start[mid] <= personsTime) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+            bloomed = low;
+            System.out.println(bloomed);
+
+            low = 0;
+            high = m-1;
+            int died = 0;
+            while (low <= high) {
+                int mid = low + (high - low)/2;
+                if(end[mid] >= personsTime) {
+                    died = mid;
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            died = low;
+
+            ans[i] = bloomed - died;
+        }
+        return ans;
+    }
+}
