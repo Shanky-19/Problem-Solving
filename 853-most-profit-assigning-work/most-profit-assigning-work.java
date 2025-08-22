d64a63da4a946bb6560e4a5802e2e8d0cd9aa5f9
Shanky-19 <banaitsanket4@gmail.com>
2025-08-22 09:19:18 +0530

    Time: 43 ms (22.42%) | Memory: 46.2 MB (20.28%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -1,6 +1,21 @@
 class Solution {
+
+    class Pair implements Comparable<Pair>{
+        int p;
+        int d;
+
+        public Pair(int p, int d) {
+            this.p = p;
+            this.d = d;
+        }
+
+        public int compareTo(Pair pair) {
+            return pair.p - this.p;
+        }
+    }
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
         // brute force
+        /*
         int m = worker.length;
         int n = profit.length;
         int ans = 0;
@@ -15,5 +30,39 @@ class Solution {
             ans += maxProfit;
         }
         return ans;
+        */
+
+        // Approach 1 : Max-Heap
+        int m = worker.length;
+        int n = profit.length;
+        int ans = 0;
+        PriorityQueue<Pair> pq 
+        = new PriorityQueue<>();
+
+        for(int i=0;i<n;i++) {
+            pq.add(new Pair(profit[i], difficulty[i]));
+        }
+
+        Integer[] workers = new Integer[m];
+        for(int i=0;i<m;i++) {
+            workers[i] = worker[i];
+        }
+
+        Arrays.sort(workers, Collections.reverseOrder());
+
+        for(int i=0;i<m;i++) {
+            int ability = workers[i];
+            while(pq.size() > 0 && pq.peek().d > ability) {
+                pq.poll();
+            }
+            if(pq.size() > 0) {
+                Pair pair = pq.peek();
+                ans += pair.p;
+            }
+        }
+        return ans;
+        
     }
+
+    
 }
