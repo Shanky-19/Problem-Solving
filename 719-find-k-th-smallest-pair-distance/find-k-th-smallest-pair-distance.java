02207c8b3f65e0bd82b1176162ca55fdf26dc3fa
Shanky-19 <banaitsanket4@gmail.com>
2025-08-23 20:28:31 +0530

    Time: 7 ms (80.29%) | Memory: 44.9 MB (21.69%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -1,6 +1,6 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
-        
+        /*
         int n = nums.length;
         int max = nums[0];
         for(int i=1;i<n;i++) {
@@ -22,6 +22,47 @@ class Solution {
             }
         }
         return 0;
+        */
 
+        // Optimal approach
+        Arrays.sort(nums);
+        int n = nums.length;
+        int low = 0;
+        int high = nums[n-1];
+        int ans = -1;
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+
+            int pairCount = findPairCount(nums, mid);
+            if(pairCount >= k) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+
+    }
+
+    private int findPairCount(int[] nums,int distance) {
+        int i = 0;
+        int j = i+1;
+        int n = nums.length;
+        int pairCount = 0;
+        while (j < n) {
+            // sorted so abs not needed
+            int dist = (nums[j]-nums[i]);
+            if(dist > distance) {
+                i++;
+            } else {
+                pairCount += j-i;
+                j++;
+            }
+            if(i == j) {
+                j++;
+            }
+        }
+        return pairCount;
     }
 }
