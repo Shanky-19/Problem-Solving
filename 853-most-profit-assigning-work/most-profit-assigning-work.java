17f3b5fc253beb37d14fefb69fa843fccdf457a6
Shanky-19 <banaitsanket4@gmail.com>
2025-08-23 08:13:24 +0530

    Time: 23 ms (46.22%) | Memory: 46.2 MB (25.69%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -13,6 +13,25 @@ class Solution {
             return pair.p - this.p;
         }
     }
+
+    class Pair2 implements Comparable<Pair2>{
+        int d;
+        int p;
+
+        public Pair2(int d, int p) {
+            this.d = d;
+            this.p = p;
+        }
+
+        public int compareTo(Pair2 pair) {
+            return this.d - pair.d;
+        }
+
+        public String toString() {
+            return ("d : " + d + " p : " + p);
+        }
+    }
+
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
         // brute force
         /*
@@ -33,6 +52,7 @@ class Solution {
         */
 
         // Approach 1 : Max-Heap
+        /*
         int m = worker.length;
         int n = profit.length;
         int ans = 0;
@@ -42,14 +62,14 @@ class Solution {
         for(int i=0;i<n;i++) {
             pq.add(new Pair(profit[i], difficulty[i]));
         }
-
-        Integer[] workers = new Integer[m];
+        // this is needed becoz Arrays.sort() 
+        // doesn't work on primitive types arr
+        // if comparator (Collections.reverOrder) is provided.
+        Integer[] workers = new Integer[m]; 
         for(int i=0;i<m;i++) {
             workers[i] = worker[i];
         }
-
         Arrays.sort(workers, Collections.reverseOrder());
-
         for(int i=0;i<m;i++) {
             int ability = workers[i];
             while(pq.size() > 0 && pq.peek().d > ability) {
@@ -61,7 +81,60 @@ class Solution {
             }
         }
         return ans;
+        */
+
+
+        // Approach 2 : Binary Search
+        int m = worker.length;
+        int n = profit.length;
+        int ans = 0;
+        List<Pair2> sortedByDiff = new ArrayList<>();
+        for(int i=0;i<n;i++) {
+            int diff = difficulty[i];
+            int prof = profit[i];
+            sortedByDiff.add(new Pair2(diff, prof));
+        }
+        Collections.sort(sortedByDiff);
+        // store max-profit till now for each idx
+        for(int i=1;i<n;i++) {
+            Pair2 p1 = sortedByDiff.get(i-1);
+            Pair2 p2 = sortedByDiff.get(i);
+
+            if(p1.p > p2.p) {
+                p2.p = p1.p;
+                sortedByDiff.set(i, p2);
+            }
+        }
+        // System.out.println(sortedByDiff);
+        for(int i=0;i<m;i++) {
+            int ability = worker[i];
+
+            int idx = binarySearch(sortedByDiff, ability);
+            if(idx != -1) {
+                int maxProfitTillIdx = sortedByDiff.get(idx).p;
+                ans += maxProfitTillIdx;
+            }
+        }
+        return ans;
         
+    }
+
+    private int binarySearch(List<Pair2> list, int ability) {
+        int low = 0;
+        int high = list.size()-1;
+        int idx = -1;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+
+            Pair2 pair = list.get(mid);
+            if(pair.d > ability) {
+                high = mid - 1;
+            }else {
+                idx = mid;
+                low = mid + 1;
+            }
+        }
+        return idx;
     }
 
     
