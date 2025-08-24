5c8c22fe139968ca060a949dd2b186e005bff4ac
Shanky-19 <banaitsanket4@gmail.com>
2025-08-24 21:13:31 +0530

    Time: 98 ms (11.61%) | Memory: 58 MB (11.53%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        int m = quantities.length;
+        Arrays.sort(quantities);
+        int low = 1;
+        int high = quantities[m-1];
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if(isPossible(quantities, n, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+        
+    }
+
+    private boolean isPossible(int[] quantities, int n, int maxProducts) {
+        int count = 0;
+        for(int val : quantities) {
+            count += (int)Math.ceil((1.0 * val)/maxProducts);
+        }
+        // store can also have 0 products
+        return (count <= n);
+    }
+}
