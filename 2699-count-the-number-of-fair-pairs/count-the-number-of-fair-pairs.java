49eea691f5ebc7aff7e5e276be6ad931d5787700
Shanky-19 <banaitsanket4@gmail.com>
2025-08-24 19:54:34 +0530

    Time: 77 ms (47.79%) | Memory: 57.3 MB (73.60%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -0,0 +1,63 @@
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        // bruce force
+        int n = nums.length;
+        long ans = 0;
+        Arrays.sort(nums);
+        for(int i=0;i<n;i++) {
+            int x = pairsSumLessThanLower(nums, i, lower);
+            int y = pairsSumeLessThanEqualToUpper(nums, i, upper);
+            // System.out.println("nums[i] " + nums[i] + " x : " + x + " y : " + y);
+            ans += (y-x);
+        }
+        return ans;
+    }
+
+    private int pairsSumLessThanLower(int[] nums, int idx, int lower) {
+        int n = nums.length;
+        int low = idx+1;
+        int high = n-1;
+        int target = lower - nums[idx];
+        int lessThanTargetIdx = -1;
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] < target) {
+                lessThanTargetIdx = mid;
+                low = mid + 1;
+            } else {
+                high = mid-1;
+            }
+        }
+        if(lessThanTargetIdx == -1) {
+            return 0;
+        }
+        int ans = lessThanTargetIdx - idx;
+        return ans;
+    }
+
+    private int pairsSumeLessThanEqualToUpper(int[] nums, int idx, int upper) {
+        int n = nums.length;
+        int low = idx+1;
+        int high = n-1;
+        int target = upper - nums[idx];
+        int lessThanEqualToTargetIdx = -1;
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] <= target) {
+                lessThanEqualToTargetIdx = mid;
+                low = mid + 1;
+            } else {
+                high = mid-1;
+            }
+        }
+        if(lessThanEqualToTargetIdx == -1) {
+            return 0;
+        }
+        int ans = lessThanEqualToTargetIdx - idx;
+        return ans;
+    }
+
+
+}
