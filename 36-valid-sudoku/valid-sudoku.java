5ceda67e68ecac8dcbd77e0e9c74773842b3af5a
Shanky-19 <banaitsanket4@gmail.com>
2025-08-30 21:55:25 +0530

    Time: 1 ms (99.87%) | Memory: 45.2 MB (7.50%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -19,7 +19,8 @@ class Solution {
         int cornery = col/3 * 3;
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                if(board[cornerx+i][cornery+j] == val && (cornerx+i)!=row && (cornery+j)!=col){
+                if(board[cornerx+i][cornery+j] == val && (cornerx+i)!=row 
+                    && (cornery+j)!=col){
                     return false;
                 }
             }
