06606c8becdb66486190a827d2d7dbe4b15d6ba0
Shanky-19 <banaitsanket4@gmail.com>
2025-10-09 18:06:52 +0530

    Time: 139 ms (49.29%) | Memory: 44.6 MB (77.46%) - LeetSync

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
@@ -3,7 +3,7 @@ class Solution {
         int n = skill.length, m = mana.length;
         long[] done = new long[n + 1];
         for (int j = 0; j < m; j++) {
-            for (int i = 0; i < n; ++i) {
+            for (int i = 0; i < n; i++) {
                 done[i + 1] = Math.max(done[i + 1], done[i]) + (long) mana[j] * skill[i];
             }
             for (int i = n - 1; i > 0; --i) {
