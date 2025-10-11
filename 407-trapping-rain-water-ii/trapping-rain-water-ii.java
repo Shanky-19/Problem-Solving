a3c75563312eb2664f8389e25d5d5fa0f07258f8
Shanky-19 <banaitsanket4@gmail.com>
2025-10-11 17:58:02 +0530

    Time: 21 ms (25.27%) | Memory: 46.5 MB (48.99%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.java
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.java
@@ -1,49 +1,54 @@
-import java.util.*;
-
+//T.C : O(m*n log(m*n))
+//S.C : O(m*n)
 class Solution {
     public int trapRainWater(int[][] heightMap) {
         int m = heightMap.length;
         int n = heightMap[0].length;
-        if (m <= 2 || n <= 2) {
-            return 0;
-        }
+
+        // Min-heap to store {height, {i, j}}
+        PriorityQueue<int[]> boundaryCells = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
 
         boolean[][] visited = new boolean[m][n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
 
-        // push borders
-        for (int i = 0; i < m; i++) {
-            pq.add(new int[]{heightMap[i][0], i, 0});
-            pq.add(new int[]{heightMap[i][n-1], i, n-1});
-            visited[i][0] = visited[i][n-1] = true;
+        // Add boundary cells (left-most, right-most, top-most, bottom-most)
+        for (int row = 0; row < m; row++) {
+            boundaryCells.offer(new int[]{heightMap[row][0], row, 0});
+            visited[row][0] = true;
+            boundaryCells.offer(new int[]{heightMap[row][n - 1], row, n - 1});
+            visited[row][n - 1] = true;
         }
-        for (int j = 0; j < n; j++) {
-            pq.add(new int[]{heightMap[0][j], 0, j});
-            pq.add(new int[]{heightMap[m-1][j], m-1, j});
-            visited[0][j] = visited[m-1][j] = true;
+        
+        for (int col = 0; col < n; col++) {
+            boundaryCells.offer(new int[]{heightMap[0][col], 0, col});
+            visited[0][col] = true;
+            boundaryCells.offer(new int[]{heightMap[m - 1][col], m - 1, col});
+            visited[m - 1][col] = true;
         }
 
-        int res = 0;
-        int[] dirs = {1, 0, -1, 0, 1};
+        int water = 0;
+        // Directions for moving to neighbors (left, right, up, down)
+        int[][] directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
-        while (!pq.isEmpty()) {
-            int[] cur = pq.remove();
-            int h = cur[0], x = cur[1], y = cur[2];
+        // Process the boundary cells in the min-heap
+        while (!boundaryCells.isEmpty()) {
+            int[] cell = boundaryCells.poll();
+            int height = cell[0];
+            int i = cell[1];
+            int j = cell[2];
 
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dirs[k];
-                int ny = y + dirs[k+1];
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n || visited[nx][ny]) {
-                    continue;
+            // Check all 4 neighbors
+            for (int[] dir : directions) {
+                int i_ = i + dir[0];
+                int j_ = j + dir[1];
+
+                if (i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && !visited[i_][j_]) {
+                    water += Math.max(height - heightMap[i_][j_], 0);
+                    boundaryCells.offer(new int[]{Math.max(height, heightMap[i_][j_]), i_, j_});
+                    visited[i_][j_] = true;
                 }
-
-                visited[nx][ny] = true;
-                if (heightMap[nx][ny] < h) {
-                    res += h - heightMap[nx][ny];
-                }
-                pq.offer(new int[]{Math.max(heightMap[nx][ny], h), nx, ny});
             }
         }
-        return res;
+
+        return water;
     }
 }
