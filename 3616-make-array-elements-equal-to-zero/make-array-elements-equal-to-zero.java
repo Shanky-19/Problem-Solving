c6097ab79efd2ca4ce9ca44e2824d5590bafb65e
Shanky-19 <banaitsanket4@gmail.com>
2025-10-28 19:55:21 +0530

    Time: 1 ms (83.13%) | Memory: 42.4 MB (48.19%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countValidSelections(int[] nums) {
+        int n = nums.length;
+        int[] rightSum = new int[n];
+        rightSum[n-1] = nums[n-1];
+        for(int i=n-2;i>=0;i--) {
+            rightSum[i] = rightSum[i+1] + nums[i];
+        }
+
+        int leftSum = 0;
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            leftSum += nums[i];
+
+            if(nums[i] == 0) {
+                if(leftSum == rightSum[i]) {
+                    ans += 2;
+                } else if(Math.abs(leftSum - rightSum[i]) == 1) {
+                    ans += 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
