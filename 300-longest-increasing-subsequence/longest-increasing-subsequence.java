cfff5dcece0609fd55c0906b7218e1187fce47e2
Shanky-19 <banaitsanket4@gmail.com>
2025-11-09 14:52:26 +0530

    Time: 87 ms (20.66%) | Memory: 237.2 MB (5.00%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,13 +1,35 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int idx = 0;
-        int prev = -1;
-        int[][] dp = new int[nums.length][nums.length];
-        for(int i=0;i<nums.length;i++) {
-            Arrays.fill(dp[i], -1);
+        // int idx = 0;
+        // int prev = -1;
+        // int[][] dp = new int[nums.length][nums.length];
+        // for(int i=0;i<nums.length;i++) {
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // int ans = helper(nums, idx, prev, dp);
+        // return ans;
+
+
+
+        // Tabulation
+        // 1. Create same dimension dp
+        // 2. Create Base case 
+        // 2. Represent problem in indexes
+        // 3. copy the recurrence
+
+        int[][] dp = new int[nums.length+1][nums.length+1];
+        for(int i=nums.length-1;i>=0;i--){
+            for(int prevIdx=i-1;prevIdx>=-1;prevIdx--){
+                int len = dp[i+1][prevIdx+1];
+                if(prevIdx == -1 || nums[i] > nums[prevIdx]){
+                    len = Math.max(len,1 + dp[i+1][i+1]);
+                }
+                dp[i][prevIdx+1] = len;
+            }
         }
-        int ans = helper(nums, idx, prev, dp);
-        return ans;
+        return dp[0][-1 + 1];
+        
+
     }
 
     private int helper(int[] nums, int idx, int prev, int[][] dp) {
