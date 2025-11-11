eecfb8d1afa734e3319e576056b2c8becefd3817
Shanky-19 <banaitsanket4@gmail.com>
2025-11-11 21:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 48.6 MB (11.91%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int i=2;
         for(int j=2;j<nums.length;j++){
             
-            if(i==0 || i==1 || nums[i-2]!=nums[j]){
+            if( nums[i-2]!=nums[j]){
                 nums[i]=nums[j];
                 i+=1;
             }
