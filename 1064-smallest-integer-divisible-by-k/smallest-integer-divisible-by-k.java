bcd18ea7a8350c39cbe01dd2f2497efd05d326b4
Shanky-19 <banaitsanket4@gmail.com>
2025-11-25 21:28:51 +0530

    Time: 2 ms (97.52%) | Memory: 42.3 MB (15.02%) - LeetSync

--- 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.java
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.java
@@ -22,7 +22,7 @@ class Solution {
             return 1;
         }
         // Numbers made of only 1s can NEVER be divisible by 2 or 5
-        if (K % 2 == 0 || K % 5 == 0 || K % 4 ==0){
+        if (K % 2 == 0 || K % 5 == 0){
             return -1;
         }
         int remain = 0;
