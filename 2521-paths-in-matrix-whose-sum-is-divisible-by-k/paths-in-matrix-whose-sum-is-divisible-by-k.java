9e3d1d049a8cb91a3088ca78dac747730d929321
Shanky-19 <banaitsanket4@gmail.com>
2025-11-28 09:17:25 +0530

    Time: 101 ms (20.98%) | Memory: 97.1 MB (45.50%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -1,35 +1,48 @@
 class Solution {
-    int m,n,k;
-    final int mod= 1_000_000_007;
-    /*NOTE: (a+b)%k == ((a%k)+(b%k))%k; */
-    Integer dp[][][];
-
-    public boolean isValid(int i, int j){
-        return i>=0 && i<m && j>=0 && j<n;
-    }
-
+    int M = 1000000007;
     public int numberOfPaths(int[][] grid, int k) {
-        m=grid.length;
-        n=grid[0].length;
-        this.k=k;
-        dp= new Integer[m][n][k];
-        return solve(0,0,0,grid);   
-    }
-
-    public int solve(int i, int j, int sum, int grid[][]){
-        if(!isValid(i,j)) return 0;
-        
-        sum=(sum+grid[i][j])%k;
-
-        if(i==m-1 && j==n-1) {
-            return sum%k==0?1:0;
+        int i = 0;
+        int j = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][][] dp = new int[m][n][k];
+        for(int[][] arr : dp) {
+            for(int[] a : arr) {
+                Arrays.fill(a, -1);
+            }
         }
-        if(dp[i][j][sum]!=null) return dp[i][j][sum]%mod;
-       
-        int down=solve(i+1,j,sum,grid);
-        int right=solve(i,j+1,sum,grid);
-
-        return dp[i][j][sum]=(down+right)%mod;
+        int sum = 0;
+        return solve(i, j, grid, m, n, k, sum, dp);
     }
 
+    private int solve(int i,int j, int[][] grid, int m, 
+                        int n, int k, int sum, int[][][] dp) {
+        
+        if(i>=m || j >= n) {
+            return 0;
+        }
+
+        if(i == m-1 && j == n-1) {
+            sum = (sum + grid[i][j])%k;
+            return dp[i][j][sum] = (sum == 0) ? 1 : 0;
+        }
+
+        if(dp[i][j][sum] != -1) {
+            return dp[i][j][sum];
+        }
+
+        int count1 = 0;
+        if(i + 1 < m) {
+            count1 = solve(i+1, j, grid, m, n, k, (sum + grid[i][j])%k, dp);
+        }
+
+        int count2 = 0;
+        if(j + 1 < n) {
+            count2 = solve(i, j+1, grid, m, n, k, (sum + grid[i][j])%k, dp);
+        }
+
+        return dp[i][j][sum] = (count1 + count2)%M;
+        
+
+    }
 }
