b043af0a0b6b57e61eddfd9c5aeefa15e009a31d
Shanky-19 <banaitsanket4@gmail.com>
2025-11-28 19:42:04 +0530

    Time: 72 ms (42.13%) | Memory: 102.2 MB (36.51%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -1,6 +1,7 @@
 class Solution {
     int M = 1000000007;
     public int numberOfPaths(int[][] grid, int k) {
+        /*
         int i = 0;
         int j = 0;
         int m = grid.length;
@@ -13,6 +14,41 @@ class Solution {
         }
         int sum = 0;
         return solve(i, j, grid, m, n, k, sum, dp);
+        */
+
+        // Tabulation
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][][] t = new int[m+1][n+1][k+1];
+
+        // Base case same as recursion
+        for (int remain = 0; remain < k; remain++) {
+            t[m-1][n-1][remain] = 
+                ((remain + grid[m-1][n-1]) % k == 0) ? 1 : 0;
+        }
+
+        // Fill bottom-up
+        for (int row = m - 1; row >= 0; row--) {
+            for (int col = n - 1; col >= 0; col--) {
+
+                for (int remain = 0; remain < k; remain++) {
+
+                    if (row == m - 1 && col == n - 1) {
+                        continue;
+                    }
+
+                    int newRemain = (remain + grid[row][col]) % k;
+
+                    long down  = t[row + 1][col][newRemain];
+                    long right = t[row][col + 1][newRemain];
+
+                    t[row][col][remain] = (int)((down + right) % M);
+                }
+            }
+        }
+
+        return t[0][0][0];
     }
 
     private int solve(int i,int j, int[][] grid, int m, 
