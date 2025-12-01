31f6d821581dad1c94c31a5b8f4b18041dc19121
Shanky-19 <banaitsanket4@gmail.com>
2025-12-01 11:37:18 +0530

    Time: 11 ms (91.10%) | Memory: 46.3 MB (33.06%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
@@ -1,48 +1,31 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
-        int n = answerKey.length();
         int i = 0;
         int j = 0;
-        int maxLength = 0;
-        int count = 0;
-        // parse 1 -> searching for T
-        while(j < n){
-            if(answerKey.charAt(j) == 'F'){
-                if(count < k){
-                    count++;
-                }else{
-                    while(answerKey.charAt(i) != 'F'){
-                        i++;
-                    }
-                    i++;
-                    // count--;
-                }
+        int countOfT = 0;
+        int countOfF = 0;
+        int n = answerKey.length();
+        int ans = 0;
+        while (j < n) {
+            char ch = answerKey.charAt(j);
+            if(ch == 'T') {
+                countOfT++;
+            } else {
+                countOfF++;
             }
-            
-            maxLength = Math.max(maxLength,j-i+1);
+
+            while (Math.min(countOfT, countOfF) > k) {
+                char c = answerKey.charAt(i);
+                if(c == 'T') {
+                    countOfT--;
+                } else {
+                    countOfF--;
+                }
+                i++;
+            }
+            ans = Math.max(ans, j-i+1);
             j++;
         }
-        i = 0;
-        j = 0;
-        count = 0;
-        // parse 2 -> searching for F
-        while(j < n){
-            
-            if(answerKey.charAt(j) == 'T'){
-                if(count < k){
-                    count++;
-                }else{
-                    while(answerKey.charAt(i) != 'T'){
-                        i++;
-                    }
-                    i++;
-                }
-            }
-            
-            maxLength = Math.max(maxLength,j-i+1);
-            j++;
-        }
-        
-        return maxLength;
+        return ans;
     }
 }
