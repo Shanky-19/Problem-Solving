acdea1c648bc0510c2ea146c681cfb223215fd95
Shanky-19 <banaitsanket4@gmail.com>
2025-12-18 09:12:50 +0530

    Time: 65 ms (85.63%) | Memory: 89 MB (36.22%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -6,24 +6,10 @@ class Solution {
         int ans = 0;
         Map<Integer, Integer> fmap = new HashMap<>();
         while(j < n) {
-            int val = nums[j];
-
-            fmap.put(val, fmap.getOrDefault(val, 0) + 1);            
-            if(fmap.get(val) > k) {
-                while(i < j && nums[i] != val) {
-                    fmap.put(nums[i], fmap.get(nums[i])-1);
-                    if(fmap.get(nums[i]) == 0) {
-                        fmap.remove(nums[i]);
-                    }
-                    i++;
-                }
-                if(fmap.size() > 0 && nums[i] == val) {
-                    fmap.put(nums[i], fmap.get(nums[i])-1);
-                    if(fmap.get(nums[i]) == 0) {
-                        fmap.remove(nums[i]);
-                    }
-                    i++;
-                }
+            fmap.put(nums[j], fmap.getOrDefault(nums[j],0)+1);
+            while (i<j && fmap.get(nums[j]) > k) {
+                fmap.put(nums[i], fmap.get(nums[i])-1);
+                i++;
             }
             ans = Math.max(ans, j-i+1);
             j++;
