9f3869999dac746c74e4a086cdc91fdd1c82c22d
Shanky-19 <banaitsanket4@gmail.com>
2026-02-08 14:57:12 +0530

    Time: 103 ms (21.88%) | Memory: 101.5 MB (11.56%) - LeetSync

---
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -0,0 +1,36 @@
+//Approach-1 (Using sliding window + heap)
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+        int n = nums.length;
+        PriorityQueue<int[]> maxPq = new PriorityQueue<>((a, b) -> b[0] - a[0]); // max-heap
+        PriorityQueue<int[]> minPq = new PriorityQueue<>((a, b) -> a[0] - b[0]); // min-heap
+
+        int i = 0; // Left boundary of the sliding window
+        int j = 0; // Right boundary of the sliding window
+        int maxLength = 0;
+
+        while (j < n) {
+            maxPq.add(new int[]{nums[j], j});
+            minPq.add(new int[]{nums[j], j});
+
+            while (maxPq.peek()[0] - minPq.peek()[0] > limit) {
+                i = Math.min(maxPq.peek()[1], minPq.peek()[1]) + 1;
+
+                while (maxPq.peek()[1] < i) {
+                    maxPq.remove();
+                }
+                while (minPq.peek()[1] < i) {
+                    minPq.remove();
+                }
+            }
+
+            // Update maxLength with the length of the current valid window
+            maxLength = Math.max(maxLength, j - i + 1);
+            j++;
+        }
+
+        return maxLength;
+    }
+}
