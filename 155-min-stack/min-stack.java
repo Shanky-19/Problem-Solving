e9d8c4bb6d529d2dc396f84a14806a11a5518968
Shanky-19 <banaitsanket4@gmail.com>
2026-03-09 20:06:43 +0530

    Time: 4 ms (99.79%) | Memory: 47.3 MB (11.23%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,13 +1,56 @@
 class MinStack {
-    Stack<Integer> s = new Stack<>(), st = new Stack<>();
+
+    LinkedList<Long> stack;
+    long minElement;
+
+    public MinStack() {
+        stack = new LinkedList<>();
+    }
+    
     public void push(int val) {
-        s.push(val);
-        if (st.isEmpty() || val <= st.peek()) st.push(val);
+        if(stack.size() == 0) {
+            stack.push((long)val);
+            minElement = val;
+        } else {
+            if(val >= minElement) {
+                stack.push((long)val);
+            } else {
+                long tamperedValue = 2L*val-minElement;
+                minElement = val;
+                stack.push(tamperedValue);
+            }
+        }
     }
+    
     public void pop() {
-        if (s.peek().equals(st.peek())) st.pop();
-        s.pop();
+        if(stack.peek() < minElement) {
+            // encountered tampered value
+            long tamperedValue = stack.peek();
+            minElement = 2*minElement - tamperedValue;
+        }
+        stack.pop();
+        
     }
-    public int top() { return s.isEmpty() ? -1 : s.peek(); }
-    public int getMin() { return st.isEmpty() ? -1 : st.peek(); }
+    
+    public int top() {
+        long top = stack.peek();
+        if(top < minElement) {
+            return (int) minElement;
+        } else {
+            return (int)top;
+        }
+    }
+    
+    public int getMin() {
+        return (int) minElement;
+    }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
