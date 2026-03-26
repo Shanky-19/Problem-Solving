51f84fbf45b6724583e18f2d4de3906e913dbb58
Sanket Banait <banaitsanket4@gmail.com>
2026-03-26 08:26:42 +0530

    Time: 7 ms (99.86%) | Memory: 90.4 MB (97.06%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -38,6 +38,8 @@ public class Solution {
     }
 
     public int amountOfTime(TreeNode root, int start) {
+        
+        /*
         Map<Integer, List<Integer>> adj = new HashMap<>();
         convert(root, -1, adj);
 
@@ -64,5 +66,32 @@ public class Solution {
         }
 
         return minutes - 1;
+        */
+
+        // One pass solution
+        dfs(root, start);
+        return result;
+        
+    }
+    int result = Integer.MIN_VALUE;
+    private int dfs(TreeNode root, int start) {
+        if(root == null) {
+            return 0;
+        }
+
+        int lh = dfs(root.left, start);
+        int rh = dfs(root.right, start);
+
+        if(root.val == start) {
+            result = Math.max(lh, rh);
+            return -1;
+        } else if(lh >=0 && rh >=0) {
+            return Math.max(lh, rh) + 1;
+        } else {
+            int d = Math.abs(lh) + Math.abs(rh);
+            result = Math.max(result, d);
+            return Math.min(lh, rh) - 1;
+        }
+
     }
 }
