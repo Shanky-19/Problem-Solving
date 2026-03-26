f413ef3847cc98f2abb1fbb183d3abd418248167
Sanket Banait <banaitsanket4@gmail.com>
2026-03-26 13:07:52 +0530

    Time: 6 ms (68.32%) | Memory: 46.6 MB (70.16%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.java
+++ 945-snakes-and-ladders/snakes-and-ladders.java
@@ -11,9 +11,13 @@ class Solution {
             
             for(int i =0; i <size; i++){
                 int x = q.poll();
-                if(x == n*n) return steps;
+                if(x == n*n) {
+                    return steps;
+                }
                 for(int k=1; k <=6; k++){
-                    if(k+x > n*n) break;
+                    if(k+x > n*n) {
+                        break;
+                    }
                     int pos[] = findCoordinates(k+x, n);
                     int r = pos[0];
                     int c = pos[1];
@@ -34,7 +38,7 @@ class Solution {
     }
         
     public int[] findCoordinates(int curr, int n) {
-        int r = n - (curr - 1) / n  -1;
+        int r = (n-1) - (curr - 1) / n  ;
         int c = (curr - 1) % n;
         if (r % 2 == n % 2) {
             return new int[]{r, n - 1 - c};
