4b564caa2db7480724b84fcf53b74f3bd4624464
Sanket Banait <banaitsanket4@gmail.com>
2026-04-18 23:00:08 +0530

    Time: 140 ms (50.89%) | Memory: 123.7 MB (32.28%) - LeetSync

---
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
@@ -0,0 +1,24 @@
+//Approach (Optimal using Monotonic Increasing Stack)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int minOperations(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int ops = 0;
+
+        for (int num : nums) {
+            while (!st.isEmpty() && st.peek() > num) {
+                st.pop();
+            }
+
+            if (num == 0) continue;
+
+            if (st.isEmpty() || st.peek() < num) {
+                st.push(num);
+                ops++;
+            }
+        }
+
+        return ops;
+    }
+}
