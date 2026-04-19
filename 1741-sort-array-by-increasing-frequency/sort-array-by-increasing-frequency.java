22a8653ff12d6c7a56aba9f55d6d90d1bb8d7c05
Sanket Banait <banaitsanket4@gmail.com>
2026-04-19 20:05:53 +0530

    Time: 7 ms (78.27%) | Memory: 45.7 MB (56.90%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -1,46 +1,30 @@
 class Solution {
+    public int[] frequencySort(int[] arr) {
 
-    class Pair implements Comparable<Pair> {
-        int freq;
-        int val;
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-        public Pair (int freq, int val) {
-            this.freq = freq;
-            this.val = val;
+        for(int n:arr){
+            map.put(n, map.getOrDefault(n, 0)+1);
         }
 
-        public int compareTo(Pair p) {
-            if(this.freq == p.freq) {
-                return p.val - this.val;
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+
+        Collections.sort(list, (a, b) -> {
+            if (map.get(a) != map.get(b)) {
+                return map.get(a) - map.get(b);
             }
-            return this.freq - p.freq;
-        }
-    }
-    public int[] frequencySort(int[] nums) {
-        Map<Integer, Integer> hm = new HashMap<>();
-        for(int val : nums) {
-            hm.put(val, hm.getOrDefault(val,0) + 1);
-        }
+            return b - a;                   
+        });
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        for(int key : hm.keySet()) {
-            int freq = hm.get(key);
-            pq.add(new Pair(freq, key));
-        }
-
-        int[] ans = new int[nums.length];
-        int idx = 0;
-        while(pq.size() > 0) {
-            Pair p = pq.remove();
-            int freq = p.freq;
-            int val = p.val;
-
-            while(freq > 0) {
-                ans[idx] = val;
-                idx++;
-                freq--;
+        int res[] = new int[arr.length];
+        int index = 0;
+        for(int n:list){
+            int freq = map.get(n);
+            for(int i = 0; i < freq; i++){
+                res[index++] = n;
             }
         }
-        return ans;
+
+        return res;
     }
 }
