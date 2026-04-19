bad199106a2307555354f211750a5762168fd152
Sanket Banait <banaitsanket4@gmail.com>
2026-04-19 19:33:21 +0530

    Time: 11 ms (96.22%) | Memory: 47.6 MB (52.08%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -20,6 +20,8 @@ class Solution {
     }
 
     public int[] topKFrequent(int[] nums, int k) {
+
+        /*
         HashMap<Integer, Integer> fmap = new HashMap<>();
         for(int val : nums){
             fmap.put(val, fmap.getOrDefault(val, 0) + 1);
@@ -44,5 +46,50 @@ class Solution {
             idx++;
         }
         return ans;
+
+        */
+
+
+        // Approach 2: Variation of Bucket Sort
+        /*
+        we will store an array where index of that array is the 
+        freq count and at that idx we will store an array of value who have that freq
+        we will then traverse from end in that array and get k elements and return them
+        */
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        }
+        ArrayList<Integer>[] freq = new ArrayList[nums.length+1];
+        // for(int i=0;i<freq.length;i++){
+        //     freq.add(new ArrayList<>());
+        // }
+
+        for(int key : hm.keySet()){
+            int f = hm.get(key);
+            // f is freq of key and it is our index in the freq arr
+            if(freq[f] == null){
+                freq[f] = new ArrayList<>();
+                freq[f].add(key);
+            }else{
+                freq[f].add(key);
+            }
+        }   
+        
+        int j = 0;
+        int[] ans = new int[k];
+        for(int i=freq.length-1;i>=0;i--){
+            if(freq[i] != null){
+                for(int val : freq[i]){
+                    if(j < k){
+                        ans[j] = val;
+                        j++;
+                    }
+                }
+            }
+        }
+        return ans;
+
+
     }
 }
