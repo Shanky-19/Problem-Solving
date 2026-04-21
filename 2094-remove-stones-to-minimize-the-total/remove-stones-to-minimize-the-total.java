f0188b2e3419e627d6b25353055e4f46e33af3e2
Sanket Banait <banaitsanket4@gmail.com>
2026-04-21 08:45:58 +0530

    Time: 883 ms (22.88%) | Memory: 96.4 MB (71.55%) - LeetSync

---
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minStoneSum(int[] piles, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
+        for(int val : piles){
+            pq.add(val);
+        }
+
+        while(k > 0){
+            int val = pq.remove();
+            val -= val/2;
+            pq.add(val);
+            k--;
+        }
+
+        int ans = 0;
+        while(pq.size() > 0){
+            ans += pq.remove();
+        }
+        return ans;
+        
+    }
+}
