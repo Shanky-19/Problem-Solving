0b932c511d0a1d149b08340ff0f2295f658d46d0
Sanket Banait <banaitsanket4@gmail.com>
2026-04-26 10:03:37 +0530

    Time: 76 ms (85.52%) | Memory: 104.5 MB (22.40%) - LeetSync

---
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.java
@@ -0,0 +1,48 @@
+class Solution {
+    class Pair implements Comparable<Pair>{
+        int val1;
+        int val2;
+
+        public Pair(int val1,int val2){
+            this.val1 = val1;
+            this.val2 = val2;
+        }
+
+        public int compareTo(Pair p){
+            return p.val2 - this.val2;
+        }
+    }
+
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+        Pair[] arr = new Pair[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            Pair p = new Pair(nums1[i],nums2[i]);
+            arr[i] = p;
+        }
+
+        Arrays.sort(arr);
+        
+        int idx = k-1;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        long sum = 0;
+        for(int i=0;i<=idx;i++){
+            pq.add(arr[i].val1);
+            sum += arr[i].val1;
+        }
+        int min = arr[k-1].val2;
+        long ans = sum * min;
+
+        for(int i=k;i<arr.length;i++){
+            int minVal = arr[i].val2;
+            int rmv = pq.remove(); // removing the lowest value so sum is maximized
+            pq.add(arr[i].val1); // if our min is arr[i].val2 then sum must contain this
+
+            sum -= rmv;
+            sum += arr[i].val1;
+
+            long tempAns = sum * minVal;
+            ans = Math.max(ans,tempAns);
+        }
+        return ans;
+    }
+}
