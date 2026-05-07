5dfb29a1bb7693558ec3ae3df3d6df1b943d8de1
Sanket Banait <banaitsanket4@gmail.com>
2026-05-07 22:12:33 +0530

    Time: 39 ms (11.04%) | Memory: 112.5 MB (5.87%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
@@ -1,37 +1,93 @@
 class Solution {
-    private static final int[][] TRANS = {
-        {-1, 1, -1, 3},
-        {0, -1, 2, -1},
-        {3, 2, -1, -1},
-        {1, -1, -1, 2},
-        {-1, 0, 3, -1},
-        {-1, -1, 1, 0}
-    };
-    private static final int[][] DIRS = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    private static final int[][] START = {{1, 3}, {0, 2}, {2, 3},
-                                          {1, 2}, {0, 3}, {0, 1}};
-
-    public boolean hasValidPath(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        if (m == 1 && n == 1) return true;
-
-        int[] s = START[grid[0][0] - 1];
-        return check(grid, s[0]) || check(grid, s[1]);
+    class Pair {
+        int i;
+        int j;
+        
+        public Pair (int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
     }
 
-    private boolean check(int[][] grid, int di) {
-        if (di == -1) return false;
-        int m = grid.length, n = grid[0].length;
-        int r = DIRS[di][0];
-        int c = DIRS[di][1];
+    private void fill(Map<Integer, List<Pair>> map) {
+        for(int i=1;i<=6;i++) {
+            map.put(i, new ArrayList<>());
+            Pair p1;
+            Pair p2;
+            if(i == 1) {
+                p1 = new Pair(0,-1);
+                p2 = new Pair(0,1);
+            } else if(i == 2) {
+                p1 = new Pair(-1,0);
+                p2 = new Pair(1,0);
+            } else if(i == 3) {
+                p1 = new Pair(0,-1);
+                p2 = new Pair(1,0);
+            } else if(i == 4) {
+                p1 = new Pair(0,1);
+                p2 = new Pair(1,0);
+            } else if(i == 5) {
+                p1 = new Pair(0,-1);
+                p2 = new Pair(-1,0);
+            } else {
+                p1 = new Pair(0,1);
+                p2 = new Pair(-1,0);
+            }
+            map.get(i).add(p1);
+            map.get(i).add(p2);
+        }
+    }
 
-        while (r >= 0 && r < m && c >= 0 && c < n) {
-            di = TRANS[grid[r][c] - 1][di];
-            if (di == -1 || r == 0 && c == 0) return false;
-            if (r == m - 1 && c == n - 1) return true;
+    boolean isValid (int i, int j, int n, int m) {
+        return (i>=0 && i<n && j>=0 && j<m);
+    }
 
-            r += DIRS[di][0];
-            c += DIRS[di][1];
+    public boolean hasValidPath(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Map<Integer, List<Pair>> map = new HashMap<>();
+        fill(map);
+
+        boolean[][] visited = new boolean[n][m];
+        boolean ans = dfs(grid, 0, 0, n, 
+                            m, map, visited);
+        return ans;
+    }
+
+    private boolean dfs(int[][] grid, int i,
+                        int j, int n, int m,
+                        Map<Integer, List<Pair>> map,
+                        boolean[][] visited) {
+
+        if(i == n-1 && j == m-1) {
+            return true;
+        }
+        visited[i][j] = true;
+
+        int point = grid[i][j];
+
+        for(Pair nbr : map.get(point)) {
+            int nextI = i + nbr.i;
+            int nextJ = j + nbr.j;
+
+            if(!isValid(nextI, nextJ, n, m) || 
+                visited[nextI][nextJ]) {
+                continue;
+            }
+
+            // The path should only follow the streets
+            // Mean, we should come be to i, j 
+            // from nextI, nextJ
+            for(Pair backDir : map.get(grid[nextI][nextJ])) {
+                if(nextI + backDir.i == i &&
+                    nextJ + backDir.j == j) {
+                    if(dfs(grid, nextI, nextJ, n, m,
+                        map, visited)) {
+                        return true;
+                    }
+                }
+            }                 
         }
         return false;
     }
