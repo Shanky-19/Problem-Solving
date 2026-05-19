5e75020d2b7cedcc242334f9a4e95523df21a815
Sanket Banait <banaitsanket4@gmail.com>
2026-05-19 22:42:56 +0530

    Time: 256 ms (57.90%) | Memory: 47.4 MB (53.87%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,22 +1,31 @@
+//Approach-2 (Optimising Approach-1)
+//T.C : O((n+k)logn)
+//S.C : O(n)
+import java.util.*;
+
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<double[]> pq = 
-        new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+        int n = arr.length;
+        PriorityQueue<double[]> pq = new PriorityQueue<>(Comparator.comparingDouble(a -> a[0]));
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                double div = (double) arr[i] / arr[j];
-                pq.add(new double[]{div, (double) arr[i], (double) arr[j]});
-                if (pq.size() > k)
-                    pq.poll();
-            }
+        for (int i = 0; i < n; i++)
+            pq.offer(new double[]{1.0 * arr[i] / arr[n - 1], (double) i, (double) (n - 1)});
+
+        int smallest = 1;
+
+        while (smallest < k) {
+            double[] vec = pq.poll();
+
+            int i = (int) vec[1];
+            int j = (int) vec[2] - 1;
+
+            pq.offer(new double[]{1.0 * arr[i] / arr[j], (double) i, (double) j});
+            smallest++;
         }
 
-        double[] ans = pq.peek();
-        int[] result = new int[2];
-        result[0] = (int) ans[1];
-        result[1] = (int) ans[2];
-        return result;
-
+        double[] vec = pq.peek();
+        int i = (int) vec[1];
+        int j = (int) vec[2];
+        return new int[]{arr[i], arr[j]};
     }
 }
