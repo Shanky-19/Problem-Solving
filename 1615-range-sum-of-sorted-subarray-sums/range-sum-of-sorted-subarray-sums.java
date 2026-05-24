da49f963e46b26c057e735671bf1a541e9b0dc6b
Sanket Banait <banaitsanket4@gmail.com>
2026-05-24 23:33:46 +0530

    Time: 80 ms (50.82%) | Memory: 46.9 MB (83.42%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,22 +1,50 @@
 class Solution {
     int mod = 1000000007;
-    public int rangeSum(int[] nums, int n, int left, int right) {
-        List<Long> list = new ArrayList<>();
-        for(int i=0;i<n;i++) {
-            long sum = nums[i];
-            list.add(sum);
-            for(int j=i+1;j<n;j++) {
-                sum += nums[j];
-                list.add(sum);
-            }
-        }
-        Collections.sort(list);
+    
+    class Pair implements Comparable<Pair>{
+        int sum;
+        int endIdx;
 
-        int ans = 0;
-        for(int i=left-1;i<=right-1;i++) {
-            ans += list.get(i);
-            ans %= mod;
+        public Pair (int sum, int endIdx) {
+            this.sum = sum;
+            this.endIdx = endIdx;
         }
-        return ans%mod;
+
+        public int compareTo(Pair p) {
+            return this.sum - p.sum;
+        }
+    }
+
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for(int i=0;i<n;i++) {
+            pq.add(new Pair(nums[i], i));
+        }
+
+        int count = 1;
+        int ans = 0;
+        while (pq.size() > 0) {
+
+            if(count > right) {
+                break;
+            }
+
+            Pair p = pq.remove();
+            int endIdx = p.endIdx;
+            int sum = p.sum;
+
+            if(count >= left && count <= right) {
+                ans = (ans + sum)%mod;
+            }
+
+            // extend subarray and add
+            
+            if(endIdx + 1 < n) {
+                pq.add(new Pair(sum + nums[endIdx+1], endIdx+1));
+            }
+            count++;
+        }
+
+        return (ans%mod);
     }
 }
