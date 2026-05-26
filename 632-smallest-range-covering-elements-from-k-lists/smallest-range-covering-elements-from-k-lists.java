bcfd42054e90f6a8ee353dda6eebc4bf66f3d066
Sanket Banait <banaitsanket4@gmail.com>
2026-05-26 21:56:51 +0530

    Time: 94 ms (77.33%) | Memory: 85.6 MB (64.84%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
@@ -1,60 +1,46 @@
+//Approach-2 (Using heap)
+//T.C : O(n*log(k))
+//S.C : O(k)
 class Solution {
-    class Pair implements Comparable<Pair>{
-        int val;
-        int idx;
-
-        public Pair(int val,int idx){
-            this.val = val;
-            this.idx = idx;
-        }
-        public int compareTo(Pair p){
-            return this.val - p.val;
-        }
-    }
-
     public int[] smallestRange(List<List<Integer>> nums) {
-        ArrayList<Pair> al = new ArrayList<>();
-        int k= nums.size();
-        for(int i=0;i<nums.size();i++){
-            List<Integer> a = nums.get(i);
-            for(int val : a){
-                Pair p = new Pair(val,i);
-                al.add(p);
-            }
+        int k = nums.size();
+        
+        // Priority Queue to store {element, listIdx, elementIdx}
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        
+        int maxEl = Integer.MIN_VALUE;
+        
+        // Initialize the priority queue with the first element of each list
+        for (int i = 0; i < k; i++) {
+            pq.offer(new int[]{nums.get(i).get(0), i, 0});
+            maxEl = Math.max(maxEl, nums.get(i).get(0));
         }
-
-        Collections.sort(al);
-        Map<Integer,Integer> hm = new HashMap<>();
-        int i = 0;
-        int j = 0;
-        int left = 0;
-        int right = 0;
-        int range = Integer.MAX_VALUE;
-        while(j < al.size()){
-            Pair p = al.get(j);
-            hm.put(p.idx,hm.getOrDefault(p.idx,0)+1);
+        
+        // Initialize result range with large numbers
+        int[] resultRange = { -1000000, 1000000 };
+        
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int minEl = curr[0];
+            int listIdx = curr[1];
+            int idx = curr[2];
             
-            if(hm.size() == k){
-                while(i < j){
-                    Pair p2 = al.get(i);
-                    if(hm.get(p2.idx) == 1){
-                        break;
-                    }else{
-                        hm.put(p2.idx,hm.get(p2.idx)-1);
-                        i++;
-                    }
-                }
-                if(range > al.get(j).val-al.get(i).val){
-                    left = al.get(i).val;
-                    right = al.get(j).val;
-                    range = right-left;
-                }
+            // Update the result range if the current range is smaller
+            if (maxEl - minEl < resultRange[1] - resultRange[0]) {
+                resultRange[0] = minEl;
+                resultRange[1] = maxEl;
             }
-            j++;
+            
+            // Try to move to the next element in the list that contained the minimum element
+            if (idx + 1 < nums.get(listIdx).size()) {
+                int nextElement = nums.get(listIdx).get(idx + 1);
+                pq.offer(new int[]{nextElement, listIdx, idx + 1});
+                maxEl = Math.max(maxEl, nextElement);  // Update maxEl with the new element
+            } else {
+                break;  // If one list is exhausted, we can't continue forming valid ranges
+            }
         }
-        int[] ans= new int[2];
-        ans[0] = left;
-        ans[1] = right;
-        return ans;
+        
+        return resultRange;
     }
 }
