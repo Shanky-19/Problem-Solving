75adcdecfbd39e7da90e3ba2d03302c3462df8df
Sanket Banait <banaitsanket4@gmail.com>
2026-05-27 21:07:37 +0530

    Time: 1 ms (89.01%) | Memory: 42.7 MB (51.34%) - LeetSync

---
+++ 1304-longest-happy-string/longest-happy-string.java
@@ -0,0 +1,86 @@
+class Solution {
+    
+    class Pair implements Comparable<Pair>{
+        char ch;
+        int freq;
+
+        public Pair (char ch, int freq) {
+            this.ch = ch;
+            this.freq = freq;
+        }
+
+        public int compareTo(Pair p) {
+            return p.freq - this.freq;
+        }
+
+        public String toString() {
+            return "ch:" + ch + " ,freq:" +freq ;
+        }
+    }
+
+    public String longestDiverseString(int a, int b, int c) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        if(a > 0) {
+            pq.add(new Pair('a', a));
+        }
+        if(b > 0) {
+            pq.add(new Pair('b', b));
+        }
+        if(c > 0) {
+            pq.add(new Pair('c', c));
+        }
+
+        char prev = 'z';
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        while (pq.size() > 0) {
+            // System.out.println(pq);
+            Pair p = pq.remove();
+            char ch = p.ch;
+            int freq = p.freq;
+
+            if(prev == ch && count == 2) {
+                if(pq.size() > 0) {
+                    // System.out.println("2nd : " + pq);
+                    Pair pair = pq.remove();
+                    char newCh = pair.ch;
+                    int newFreq = pair.freq;
+                    // System.out.println("2.1 nd : " + pq);
+                    sb.append(newCh);
+                    prev = newCh;
+                    count = 1;
+
+                    newFreq--;
+                    if(newFreq > 0) {
+                        // System.out.println("3rd : " + newFreq);
+                        pq.add(new Pair(newCh, newFreq));
+                    }
+                    pq.add(p);
+                } else {
+                    return sb.toString();
+                }
+            } else if(prev == ch) {
+                prev = ch;
+                count++;
+                sb.append(ch);
+
+                // freq--;
+                p.freq--;
+                if(p.freq > 0) {
+                    pq.add(p);
+                }
+            } else {
+                prev = ch;
+                count = 1;
+                sb.append(ch);
+                p.freq--;
+                if(p.freq > 0) {
+                    pq.add(p);
+                }
+            }
+
+        }
+        return sb.toString();
+    }
+}
