f7672a76db07dd690565a27d99ebee8ad3ec6d7d
Sanket Banait <banaitsanket4@gmail.com>
2026-06-22 22:35:58 +0530

    Time: 79 ms (55.73%) | Memory: 173 MB (24.41%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -0,0 +1,50 @@
+class Solution {
+    class Pair {
+        int win;
+        int loss;
+        public Pair (int win, int loss) {
+            this.win = win;
+            this.loss = loss;
+        }
+    }
+    public List<List<Integer>> findWinners(int[][] matches) {
+        int n = matches.length;
+        HashMap<Integer, Pair> hm = new HashMap<>();
+        for(int[] match : matches) {
+            int winner = match[0];
+            int loser  = match[1];
+
+            if(!hm.containsKey(winner)) {
+                hm.put(winner, new Pair(1, 0));
+            } else {
+                Pair p = hm.get(winner);
+                p.win += 1;
+                hm.put(winner, p);
+            }
+
+            if(!hm.containsKey(loser)) {
+                hm.put(loser, new Pair(0, 1));
+            } else {
+                Pair p = hm.get(loser);
+                p.loss += 1;
+                hm.put(loser, p);
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> allWins = new ArrayList<>();
+        List<Integer> oneLose = new ArrayList<>();
+        for(int key : hm.keySet()) {
+            Pair p = hm.get(key);
+            if(p.loss == 0) {
+                allWins.add(key);
+            } else if(p.loss == 1){
+                oneLose.add(key);
+            }
+        }
+        Collections.sort(allWins);
+        Collections.sort(oneLose);
+        ans.add(allWins);
+        ans.add(oneLose);
+        return ans;
+    }
+}
