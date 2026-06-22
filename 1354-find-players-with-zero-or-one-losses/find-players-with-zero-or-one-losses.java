44bc2ea726e8935e1c6887c0e4980feb2ad0ab4b
Sanket Banait <banaitsanket4@gmail.com>
2026-06-22 22:39:51 +0530

    Time: 93 ms (27.71%) | Memory: 166.3 MB (58.07%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -1,55 +1,33 @@
+//T.C : O(n*logn)
+//S.C : O(n)
 class Solution {
-    
-    class Pair {
-        int win;
-        int loss;
-        public Pair (int win, int loss) {
-            this.win = win;
-            this.loss = loss;
-        }
-    }
-
     public List<List<Integer>> findWinners(int[][] matches) {
-        int n = matches.length;
-        HashMap<Integer, Pair> hm = new HashMap<>();
-        for(int[] match : matches) {
-            int winner = match[0];
-            int loser  = match[1];
+        Map<Integer, Integer> lost = new HashMap<>();
 
-            if(!hm.containsKey(winner)) {
-                hm.put(winner, new Pair(1, 0));
-            } else {
-                Pair p = hm.get(winner);
-                p.win += 1;
-                hm.put(winner, p);
-            }
-
-            if(!hm.containsKey(loser)) {
-                hm.put(loser, new Pair(0, 1));
-            } else {
-                Pair p = hm.get(loser);
-                p.loss += 1;
-                hm.put(loser, p);
-            }
-
+        for (int[] it : matches) {
+            int lose = it[1];
+            lost.put(lose, lost.getOrDefault(lose, 0) + 1);
         }
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> allWins = new ArrayList<>();
-        List<Integer> oneLose = new ArrayList<>();
-        for(int key : hm.keySet()) {
-            Pair p = hm.get(key);
-            if(p.loss == 0) {
-                allWins.add(key);
-            } else if(p.loss == 1){
-                oneLose.add(key);
+
+        List<Integer> notLost = new ArrayList<>();
+        List<Integer> oneLos = new ArrayList<>();
+
+        for (int[] it : matches) {
+            int lose = it[1];
+            int win = it[0];
+
+            if (lost.get(lose) == 1) {
+                oneLos.add(lose);
+            }
+            if (!lost.containsKey(win)) {
+                notLost.add(win);
+                lost.put(win, 2);
             }
         }
 
-        Collections.sort(allWins);
-        Collections.sort(oneLose);
+        Collections.sort(notLost);
+        Collections.sort(oneLos);
 
-        ans.add(allWins);
-        ans.add(oneLose);
-        return ans;
+        return Arrays.asList(notLost, oneLos);
     }
 }
