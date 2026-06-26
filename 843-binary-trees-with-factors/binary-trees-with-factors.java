7f2847410f01cb609f6f345b9615cfa725394f8e
Sanket Banait <banaitsanket4@gmail.com>
2026-06-26 08:32:31 +0530

    Time: 33 ms (12.64%) | Memory: 46.8 MB (61.49%) - LeetSync

--- 843-binary-trees-with-factors/binary-trees-with-factors.java
+++ 843-binary-trees-with-factors/binary-trees-with-factors.java
@@ -1,32 +1,31 @@
 class Solution {
     final int mod = (int)(1e9+7);
     public int numFactoredBinaryTrees(int[] arr) {
-        Arrays.sort(arr);
-        Map<Integer, Long> hmp = new HashMap<>();
-        for(int x: arr)
-            hmp.put(x, 1L);
-        
         int n = arr.length;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                int prod = arr[i];
-                int x1 = arr[j], x2 = prod/arr[j];
-                if(prod%arr[j] == 0 && hmp.containsKey(x2))
-                {
-                    long res = (hmp.get(x1)*hmp.get(x2))%mod;
-                    long val = hmp.get(prod);
+
+        Arrays.sort(arr);
+        Map<Integer, Long> hm = new HashMap<>();
+        for(int val: arr) {
+            hm.put(val, 1L);
+        }
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<i;j++) {
+                int root = arr[i];
+                int leftChild = arr[j];
+                int rightChild = root/arr[j];
+                if(root%arr[j] == 0 && hm.containsKey(rightChild)) {
+                    long res = (hm.get(leftChild)*hm.get(rightChild))%mod;
+                    long val = hm.get(root);
                     val = (val + res)%mod;
-                    hmp.put(prod, val);
+                    hm.put(root, val);
                 }
             }
         }
         
         long ans = 0;
-        for(Integer x: hmp.keySet())
-            ans = (ans + hmp.get(x))%mod;
-        
+        for(Integer x: hm.keySet()) {
+            ans = (ans + hm.get(x))%mod;
+        }
         return (int)ans;
     }
 }
