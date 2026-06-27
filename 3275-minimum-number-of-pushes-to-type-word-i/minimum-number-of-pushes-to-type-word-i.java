0aba9853e1b375317a898ae0dceb42a1e1733736
Sanket Banait <banaitsanket4@gmail.com>
2026-06-27 11:45:32 +0530

    Time: 22 ms (3.64%) | Memory: 45.1 MB (6.07%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimumPushes(String word) {
+       Map<Character, Integer> hm = new HashMap<>();
+       int currPush = 1;
+       int currButton = 2;
+       int pushes = 0;
+       for(char ch : word.toCharArray()) {
+            if(hm.containsKey(ch)) {
+                pushes += hm.get(ch);
+            } else {
+                if(currButton == 10) {
+                    currButton = 2;
+                    currButton++;
+                    currPush++;
+                } else {
+                    currButton++;
+                }
+                
+                hm.put(ch, currPush);
+                pushes += hm.get(ch);
+            }
+            System.out.println(ch + " " + hm.get(ch));
+       }
+       return pushes;
+
+    }
+}
