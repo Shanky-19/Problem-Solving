d8104e121fd85c5ebd4496bbd2f360365c6b53cf
Sanket Banait <banaitsanket4@gmail.com>
2026-06-28 16:35:08 +0530

    Time: 114 ms (45.34%) | Memory: 100.5 MB (7.84%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,59 +1,33 @@
+//Approach - Using map
+//T.C : O(n), where n = number of words
+//S.C : O(n)
 class Solution {
-
-
-    public boolean isPalindrome(String s) {
-        int i = 0;
-        int j = s.length()-1;
-        while(i < j) {
-            if(s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
-
     public int longestPalindrome(String[] words) {
         Map<String, Integer> map = new HashMap<>();
-        for(String word : words) {
-            map.put(word, map.getOrDefault(word, 0) + 1);
-        }
-        // System.out.println(map);
-        int lengthSoFar = 0;
-        int count = 0;
-        Map<String, Integer> map2 = new HashMap<>();
-        for(String key : map.keySet()) {
-            String reverseKey = new StringBuilder(key).reverse().toString();
-            if(reverseKey.equals(key)) {
-                count++;
-                map2.put(key, map.get(key));
-                continue;
-            }else if(map.containsKey(reverseKey)) {
-                int freq2 = map.get(reverseKey);
-                int freq1 = map.get(key);
-                int minFreq = Math.min(freq1, freq2);
-                lengthSoFar += (2 * minFreq) + (2 * minFreq);
-                map.put(reverseKey, 0);
-            }
-            map.put(key, 0);
-        }
-        boolean allowed = true;
-        // System.out.println(map2);
-        // System.out.println(count);
-        for(String key : map2.keySet()) {
-            int freq = map2.get(key);
-            if(freq%2 == 1) {
-                if(allowed) {
-                    lengthSoFar += 2;
-                    allowed = false;
-                }
-            }
+        int result = 0;
 
-            int pairs = freq/2;
-            lengthSoFar += (2 * pairs * 2);
+        for (String word : words) {
+            String reversed = new StringBuilder(word).reverse().toString();
+
+            if (map.getOrDefault(reversed, 0) > 0) {
+                result += 4;
+                map.put(reversed, map.get(reversed) - 1);
+            } else {
+                map.put(word, map.getOrDefault(word, 0) + 1);
+            }
         }
-        
-        return lengthSoFar ;
+
+        // Check for a word with both characters same to use in the center
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String word = entry.getKey();
+            int count = entry.getValue();
+
+            if (word.charAt(0) == word.charAt(1) && count > 0) {
+                result += 2;
+                break;
+            }
+        }
+
+        return result;
     }
 }
