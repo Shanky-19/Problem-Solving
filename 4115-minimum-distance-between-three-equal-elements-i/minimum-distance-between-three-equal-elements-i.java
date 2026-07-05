243ca9b409ee6ecd5abc624605487e70ad838de6
Sanket Banait <banaitsanket4@gmail.com>
2026-07-05 21:49:54 +0530

    Time: 3 ms (80.51%) | Memory: 44.3 MB (64.94%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
@@ -1,20 +1,26 @@
 class Solution {
     public int minimumDistance(int[] nums) {
-        int len = nums.length;
-        int[] last2 = new int[len];
-        int res = 200;
-
-        for (int i = 0; i < len; i++) {
-            int val = nums[i] - 1;
-            int pos = i + 1, pack = last2[val];
-            int old = pack & 255, cur = pack >> 8;
-
-            last2[val] = cur | (pos << 8);
-
-            if (old > 0)
-                res = Math.min(res, (pos - old) << 1);
+        // Brute Force
+    
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<n-2;i++) {
+            for(int j=i+1;j<n-1;j++) {
+                if(nums[i] == nums[j]) {
+                    for(int k=j+1;k<n;k++) {
+                        if(nums[j] == nums[k]) {
+                            int potentialAns = (Math.abs(i-j) + 
+                                                Math.abs(j-k) + Math.abs(i-k));
+                            ans = Math.min(ans, potentialAns); 
+                        }
+                    }
+                }
+            }
         }
+        if(ans == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return ans;
 
-        return res == 200 ? -1 : res;
     }
 }
