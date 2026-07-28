3441da7823ee43d6bd530efbf21de55b3bdcde7d
Sanket Banait <banaitsanket4@gmail.com>
2026-07-28 09:30:23 +0530

    Time: 27 ms (75.10%) | Memory: 48 MB (88.35%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,32 +1,18 @@
 class Solution {
     public String smallestPalindrome(String s) {
         int n = s.length();
-        Map<Character, Integer> tmap = new TreeMap<>();
-        for(int i=0;i<n;i++) {
-            char ch = s.charAt(i);
-            tmap.put(ch, tmap.getOrDefault(ch, 0)+1);
+        char[] firstHalf = s.substring(0, n/2).toCharArray();
+        Arrays.sort(firstHalf);
+        char[] ans = new char[n];
+        for(int i=0;i<n/2;i++) {
+            ans[i] = firstHalf[i];
+            ans[n-1-i] = firstHalf[i];
         }
 
-        // System.out.println(tmap);
-
-        char[] ans = new char[n];
-        int j = 0;
-        for(char ch : tmap.keySet()) {
-            int freq = tmap.get(ch);
-            while(freq > 1) {
-                freq-=2;
-                tmap.put(ch, freq);
-                ans[j] = ch;
-                ans[n-1-j] = ch;
-                j++;
-            }
-            if(freq == 1) {
-                ans[n/2] = ch;
-            }
+        if(n%2 != 0) {
+            ans[n/2] = s.charAt(n/2);
         }
 
         return new String(ans);
-
-
     }
 }
