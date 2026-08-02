562dc13c0a8ab4e435576364dfc782d6eee1c777
Sanket Banait <banaitsanket4@gmail.com>
2026-08-02 11:29:23 +0530

    Time: 41 ms (31.11%) | Memory: 42.8 MB (57.72%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,14 +1,33 @@
 class Solution {
+
+    private int solve (int l, int r, int[] nums) {
+        if(l > r) {
+            return 0;
+        }
+
+        int leftChose = nums[l] + Math.min(solve(l+2, r, nums), solve(l+1, r-1, nums));
+
+        int rightChose = nums[r] + Math.min(solve(l, r-2, nums), solve(l+1, r-1, nums));
+
+        return Math.max(leftChose, rightChose);
+    }
+
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        if (n % 2 == 0) return true;
-        
-        int[] dp = nums.clone();
-        for (int i = n - 2; i >= 0; --i) {
-            for (int j = i + 1; j < n; ++j) {
-                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
-            }
+        if (n % 2 == 0) {
+            return true;
         }
-        return dp[n - 1] >= 0;
+        
+        int total = 0;
+        for(int val : nums) {
+            total += val;
+        }
+        
+        int l = 0;
+        int r = n-1;
+        int player1Score = solve(l, r, nums);
+        int player2Score = total - player1Score;
+
+        return player1Score >= player2Score;
     }
 }
