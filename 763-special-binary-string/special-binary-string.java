8ab2b59fa30fe023963c5de69c37adf20bb101fb
Sanket Banait <banaitsanket4@gmail.com>
2026-08-15 21:34:35 +0530

    Time: 2 ms (99.37%) | Memory: 43.2 MB (55.39%) - LeetSync

--- 763-special-binary-string/special-binary-string.java
+++ 763-special-binary-string/special-binary-string.java
@@ -1,23 +1,34 @@
+//Approach : (Recursion)
+//T.C : ~O(n^2)
+//S.C : O(n) stack space and to store special substrings
 class Solution {
     public String makeLargestSpecial(String s) {
-        int count = 0, i = 0;
-        List<String> res = new ArrayList<>();
-        
-        for (int j = 0; j < s.length(); j++) {
-            // Track balance: +1 for '1', -1 for '0'
-            if (s.charAt(j) == '1') count++;
-            else count--;
-            
-            // Found a balanced chunk when count returns to 0
+
+        List<String> specials = new ArrayList<>();
+        int count = 0;
+        int start = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            count += (s.charAt(i) == '1') ? 1 : -1;
+
             if (count == 0) {
-                // Recursively maximize inner part, wrap with 1...0
-                res.add('1' + makeLargestSpecial(s.substring(i + 1, j)) + '0');
-                i = j + 1; // Move to next potential chunk
+                String inner = s.substring(start + 1, i);
+
+                String processed = "1" + makeLargestSpecial(inner) + "0";
+                specials.add(processed);
+
+                start = i + 1;
             }
         }
-        
-        // Sort chunks in descending order for largest arrangement
-        Collections.sort(res, Collections.reverseOrder());
-        return String.join("", res);
+
+        Collections.sort(specials, Collections.reverseOrder());
+
+
+        StringBuilder result = new StringBuilder();
+        for (String str : specials) {
+            result.append(str);
+        }
+
+        return result.toString();
     }
 }
