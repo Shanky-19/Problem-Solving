9d0b812afa6b68db75b9a14a624368d82f9d3a10
Sanket Banait <banaitsanket4@gmail.com>
2026-08-16 10:33:40 +0530

    Time: 139 ms (47.76%) | Memory: 42.7 MB (88.45%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,33 +1,35 @@
 class Solution {
 
-    public boolean dfs(int i,int j,char[][] board,String word,int idx,boolean[][] visited){
+    public boolean dfs(int i,int j,char[][] board,
+                        String word,int idx){
         if(idx == word.length()){
             return true;
         }
         
-        if(i<0 || j< 0 || i==board.length || j == board[0].length || visited[i][j] || board[i][j] != word.charAt(idx)){
+        if(i<0 || j< 0 || i==board.length || j == board[0].length 
+            || board[i][j] == '#' || board[i][j] != word.charAt(idx)){
             return false;
         }
-
-        visited[i][j] = true;
-        boolean ans1 = dfs(i+1,j,board,word,idx+1,visited);
+        char tempChar = board[i][j];
+        board[i][j] = '#';
+        boolean ans1 = dfs(i+1,j,board,word,idx+1);
         if(ans1){
             return true;
         }
-        boolean ans2 = dfs(i-1,j,board,word,idx+1,visited);
+        boolean ans2 = dfs(i-1,j,board,word,idx+1);
         if(ans2){
             return true;
         }
-        boolean ans3 = dfs(i,j+1,board,word,idx+1,visited);
+        boolean ans3 = dfs(i,j+1,board,word,idx+1);
         if(ans3){
             return true;
         }
-        boolean ans4 = dfs(i,j-1,board,word,idx+1,visited);
+        boolean ans4 = dfs(i,j-1,board,word,idx+1);
         if(ans4){
             return true;
         }
 
-        visited[i][j] = false;
+        board[i][j] = tempChar;
         return false;
 
     }
@@ -35,11 +37,10 @@ class Solution {
         int n = board.length;
         int m = board[0].length;
         int idx = 0;
-        boolean[][] visited = new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j] == word.charAt(idx)){
-                    boolean ans = dfs(i,j,board,word,idx,visited);
+                    boolean ans = dfs(i,j,board,word,idx);
                     if(ans){
                         return true;
                     }
