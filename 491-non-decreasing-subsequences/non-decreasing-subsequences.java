d0ec86228977425f8a4fc2b020142907d70ddaa5
Sanket Banait <banaitsanket4@gmail.com>
2026-08-16 12:24:42 +0530

    Time: 16 ms (39.12%) | Memory: 53.4 MB (31.15%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.java
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.java
@@ -1,7 +1,7 @@
 class Solution {
 
-    private void solve (int idx, int prev, int[] nums,
-            List<Integer> al, Set<List<Integer>> ans) {
+    private void solve (int idx, int[] nums,
+                        List<Integer> al, Set<List<Integer>> ans) {
         
         if(idx == nums.length) {
             if(al.size() >= 2) {
@@ -10,37 +10,26 @@ class Solution {
             return;
         }
 
-        if(prev == -101) {
-            // pick 
+        // pick 
+        if(al.size() == 0 || al.get(al.size()-1) <= nums[idx]) {
             al.add(nums[idx]);
-            solve(idx+1, nums[idx], nums, al, ans);
+            solve(idx+1, nums, al, ans);
             al.remove(al.size() - 1);
-
-            // not pick
-            solve(idx+1, prev, nums, al, ans);
-        } else {
-
-            // pick 
-            if(prev <= nums[idx]) {
-                al.add(nums[idx]);
-                solve(idx+1, nums[idx], nums, al, ans);
-                al.remove(al.size() - 1);
-            }
-
-            // not pick
-            solve(idx +1, prev, nums, al, ans);
         }
+
+        // not pick
+        solve(idx+1, nums, al, ans);
+        
 
     }
 
     public List<List<Integer>> findSubsequences(int[] nums) {
         int n = nums.length;
         Set<List<Integer>> ans = new HashSet<>();
-        int prev = -101; // as nums[i] <= 100 && nums[i] >= -100
 
         List<Integer> al = new ArrayList<>();
         int idx = 0;
-        solve(idx, prev, nums, al, ans);
+        solve(idx, nums, al, ans);
 
         return new ArrayList<>(ans);
         
