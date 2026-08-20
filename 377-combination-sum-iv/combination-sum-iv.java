959343feb3dbedeeed64a2edda17016bdf8fdd3b
Sanket Banait <banaitsanket4@gmail.com>
2026-08-20 22:41:17 +0530

    Time: 1 ms (67.76%) | Memory: 42.6 MB (78.29%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.java
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -3,6 +3,7 @@ class Solution {
         int n = nums.length;
         int[] dp = new int[target+1];
         dp[0] = 1;
+
         for(int j=1;j<=target;j++){
             for(int i=0;i<n;i++){  
                 if(j >= nums[i]){
@@ -10,6 +11,7 @@ class Solution {
                 }
             }
         }
+        
         return dp[target];
     }
 }
