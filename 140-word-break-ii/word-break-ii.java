646cee323f1444859014fb30a9a8a6768bdffe80
Sanket Banait <banaitsanket4@gmail.com>
2026-08-22 15:36:38 +0530

    Time: 2 ms (64.35%) | Memory: 43.3 MB (64.37%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public void helper(String s,Set<String> set,StringBuilder sb,List<String> ans){
+        if(s.length() == 0){
+            sb.deleteCharAt(sb.length()-1); // to remove extra space which is added at last
+            ans.add(sb.toString());
+            return;
+        }
+
+        // we have to make a cut
+        for(int i=0;i<s.length();i++){
+            String prefix = s.substring(0,i+1);
+            String remainingString = s.substring(i+1);
+            if(set.contains(prefix)){
+            // as we cannot remove from sb the last prefix added we have to use temp for backtracking
+                StringBuilder temp = new StringBuilder(sb);
+                temp.append(prefix+" ");
+                helper(remainingString,set,temp,ans);
+            }
+        }
+
+    }
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        List<String> ans = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        for(String word: wordDict){
+            set.add(word);
+        }
+        StringBuilder sb = new StringBuilder();
+        helper(s,set,sb,ans);
+        return ans;
+    }
+}
