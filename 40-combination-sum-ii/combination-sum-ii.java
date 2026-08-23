7ed43b0c16256f7d13e0dcad4ae2c8235d500046
Sanket Banait <banaitsanket4@gmail.com>
2026-08-23 10:11:59 +0530

    Time: 5 ms (99.19%) | Memory: 45.3 MB (40.22%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public void helper(int[] candidates, int target,int idx,List<Integer> al,List<List<Integer>> ans){
+        if(target == 0){
+            ans.add(new ArrayList<>(al));
+            return;
+        }
+
+        for(int i=idx;i<candidates.length;i++){
+            if(i>idx && candidates[i]==candidates[i-1]){
+                continue;
+            }
+
+            if(candidates[i]> target){
+                break;
+            }
+
+            al.add(candidates[i]);
+            helper(candidates,target-candidates[i],i+1,al,ans);
+            al.remove(al.size()-1);
+        }
+       
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);// with this we can avoid the condition like [1,7] and [7,1] -> added once
+        int idx = 0;
+        List<Integer> al = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(candidates,target,idx,al,ans);
+        return ans;
+    }
+}
