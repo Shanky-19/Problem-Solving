57daa7e55be4903a94e9c8844e71d5e3b894b501
Sanket Banait <banaitsanket4@gmail.com>
2026-08-25 00:25:57 +0530

    Time: 582 ms (34.19%) | Memory: 46.9 MB (62.58%) - LeetSync

---
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.java
@@ -0,0 +1,71 @@
+class Solution {
+    String result = "";
+
+    // Check if seq * k is a subsequence of s
+    private boolean isSubsequence(String s, String sub, int k) {
+        int i = 0, j = 0, len = sub.length(), n = s.length();
+
+        while (i < n && j < k * len) {
+            if (s.charAt(i) == sub.charAt(j % len)) {
+                j++;
+            }
+            i++;
+        }
+
+        return j == k * len;
+    }
+
+    private void solve(String s, StringBuilder curr, boolean[] canUse,
+                        int[] maxFreqInOneSubsequence,int k, int maxLen) {
+
+        if (curr.length() > maxLen) {
+            return;
+        }
+
+        String currStr = curr.toString();
+        if ((curr.length() > result.length() || 
+             (curr.length() == result.length() && currStr.compareTo(result) > 0)) &&
+            isSubsequence(s, currStr, k)) {
+            result = currStr;
+        }
+
+        for (int i = 25; i >= 0; i--) { // from 'z' to 'a' for lexicographically larger result
+            if (!canUse[i] || maxFreqInOneSubsequence[i] == 0) {
+                continue;
+            }
+
+            curr.append((char) (i + 'a'));
+            maxFreqInOneSubsequence[i]--;
+
+            solve(s, curr, canUse, maxFreqInOneSubsequence, k, maxLen);
+
+            curr.deleteCharAt(curr.length() - 1);
+            maxFreqInOneSubsequence[i]++;
+        }
+    }
+
+    public String longestSubsequenceRepeatedK(String s, int k) {
+        int n = s.length();
+        int[] freqMap = new int[26];
+        for(char ch : s.toCharArray()) {
+            freqMap[ch - 'a']++;
+        }
+
+        boolean[] canUse = new boolean[26];
+        int[] maxFreqInOneSubsequence = new int[26];
+        for(int i=0;i<26;i++) {
+            if(freqMap[i] >= k) {
+                canUse[i] = true;
+                maxFreqInOneSubsequence[i] = freqMap[i]/k; // at-most
+            }
+        }
+
+        int maxLen = n/k;
+
+        StringBuilder curr = new StringBuilder();
+        solve(s, curr, canUse, maxFreqInOneSubsequence, k, maxLen);
+        return result;
+    }
+
+
+}
