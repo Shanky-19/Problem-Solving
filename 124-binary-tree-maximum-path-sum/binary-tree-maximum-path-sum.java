7fd0bf2c2893773c8ced89dc2280214a22e7f4d6
Sanket Banait <banaitsanket4@gmail.com>
2026-09-07 19:05:25 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (37.45%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -19,12 +19,16 @@ class Solution {
         if(root==null) {
             return 0;
         }
+
         int left=Math.max(0,helper(root.left));
+
         int right=Math.max(0,helper(root.right));
+
         res=Math.max(root.val+left+right,res);
         return root.val+Math.max(left,right);
     }
     public int maxPathSum(TreeNode root) {
+
         helper(root);
         return res;
     }
