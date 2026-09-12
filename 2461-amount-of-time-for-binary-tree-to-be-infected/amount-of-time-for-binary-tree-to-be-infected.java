cc7e4eb026a0b4b7020f740e781efb52a39e8ab5
Sanket Banait <banaitsanket4@gmail.com>
2026-09-12 15:11:52 +0530

    Time: 8 ms (97.91%) | Memory: 90.5 MB (94.90%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -18,30 +18,28 @@
 //S.C : O(n) - storing all nodes in graph
 public class Solution {
 
-    public void convert(TreeNode current, int parent, 
-                        Map<Integer, List<Integer>> adj) {
+    public void convert(TreeNode current, int parent, Map<Integer, List<Integer>> adj) {
         if (current == null) {
             return;
         }
 
         if (parent != -1) {
-            adj.computeIfAbsent(current.val, 
-                        k -> new ArrayList<>()).add(parent);
+            adj.computeIfAbsent(current.val, k -> new ArrayList<>()).add(parent);
         }
 
         if (current.left != null) {
-            adj.computeIfAbsent(current.val, 
-                        k -> new ArrayList<>()).add(current.left.val);
+            adj.computeIfAbsent(current.val, k -> new ArrayList<>()).add(current.left.val);
         }
         if (current.right != null) {
-            adj.computeIfAbsent(current.val, 
-                        k -> new ArrayList<>()).add(current.right.val);
+            adj.computeIfAbsent(current.val, k -> new ArrayList<>()).add(current.right.val);
         }
         convert(current.left, current.val, adj);
         convert(current.right, current.val, adj);
     }
 
     public int amountOfTime(TreeNode root, int start) {
+        
+        /*
         Map<Integer, List<Integer>> adj = new HashMap<>();
         convert(root, -1, adj);
 
@@ -57,8 +55,7 @@ public class Solution {
             while (n-- > 0) {
                 int curr = que.poll();
 
-                for (int ngbr : adj.getOrDefault(curr, 
-                        Collections.emptyList())) {
+                for (int ngbr : adj.getOrDefault(curr, Collections.emptyList())) {
                     if (!visited.contains(ngbr)) {
                         que.add(ngbr);
                         visited.add(ngbr);
@@ -69,5 +66,32 @@ public class Solution {
         }
 
         return minutes - 1;
+        */
+
+        // One pass solution
+        dfs(root, start);
+        return result;
+        
+    }
+    int result = Integer.MIN_VALUE;
+    private int dfs(TreeNode root, int start) {
+        if(root == null) {
+            return 0;
+        }
+
+        int lh = dfs(root.left, start);
+        int rh = dfs(root.right, start);
+
+        if(root.val == start) {
+            result = Math.max(lh, rh);
+            return -1;
+        } else if(lh >=0 && rh >=0) {
+            return Math.max(lh, rh) + 1;
+        } else {
+            int d = Math.abs(lh) + Math.abs(rh);
+            result = Math.max(result, d);
+            return Math.min(lh, rh) - 1;
+        }
+
     }
 }
