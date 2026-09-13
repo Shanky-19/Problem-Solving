2ed7dbefee13e3f0897fe12955a033458fc45a2d
Sanket Banait <banaitsanket4@gmail.com>
2026-09-13 09:40:00 +0530

    Time: 7 ms (99.80%) | Memory: 90.3 MB (97.71%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -13,85 +13,32 @@
  *     }
  * }
  */
-//Approach-1 (Convert tree to graph)
-//T.C : O(n) - visiting all nodes
-//S.C : O(n) - storing all nodes in graph
 public class Solution {
+    private int result = Integer.MIN_VALUE;
 
-    public void convert(TreeNode current, int parent, Map<Integer, List<Integer>> adj) {
-        if (current == null) {
-            return;
-        }
-
-        if (parent != -1) {
-            adj.computeIfAbsent(current.val, k -> new ArrayList<>()).add(parent);
-        }
-
-        if (current.left != null) {
-            adj.computeIfAbsent(current.val, k -> new ArrayList<>()).add(current.left.val);
-        }
-        if (current.right != null) {
-            adj.computeIfAbsent(current.val, k -> new ArrayList<>()).add(current.right.val);
-        }
-        convert(current.left, current.val, adj);
-        convert(current.right, current.val, adj);
-    }
-
-    public int amountOfTime(TreeNode root, int start) {
-        
-        /*
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-        convert(root, -1, adj);
-
-        Queue<Integer> que = new LinkedList<>();
-        que.add(start);
-        Set<Integer> visited = new HashSet<>();
-        visited.add(start);
-        int minutes = 0;
-
-        while (!que.isEmpty()) {
-            int n = que.size();
-
-            while (n-- > 0) {
-                int curr = que.poll();
-
-                for (int ngbr : adj.getOrDefault(curr, Collections.emptyList())) {
-                    if (!visited.contains(ngbr)) {
-                        que.add(ngbr);
-                        visited.add(ngbr);
-                    }
-                }
-            }
-            minutes++;
-        }
-
-        return minutes - 1;
-        */
-
-        // One pass solution
-        dfs(root, start);
-        return result;
-        
-    }
-    int result = Integer.MIN_VALUE;
-    private int dfs(TreeNode root, int start) {
-        if(root == null) {
+    public int solve(TreeNode root, int start) {
+        if (root == null) {
             return 0;
         }
 
-        int lh = dfs(root.left, start);
-        int rh = dfs(root.right, start);
+        int LH = solve(root.left, start);
+        int RH = solve(root.right, start);
 
-        if(root.val == start) {
-            result = Math.max(lh, rh);
+        if (root.val == start) {
+            result = Math.max(LH, RH);
             return -1;
-        } else if(lh >=0 && rh >=0) {
-            return Math.max(lh, rh) + 1;
+        } else if (LH >= 0 && RH >= 0) {
+            return Math.max(LH, RH) + 1;
         } else {
-            int d = Math.abs(lh) + Math.abs(rh);
+            int d = Math.abs(LH) + Math.abs(RH);
             result = Math.max(result, d);
-            return Math.min(lh, rh) - 1;
-        }
 
+            return Math.min(LH, RH) - 1;
+        }
+    }
+
+    public int amountOfTime(TreeNode root, int start) {
+        solve(root, start);
+        return result;
     }
 }
