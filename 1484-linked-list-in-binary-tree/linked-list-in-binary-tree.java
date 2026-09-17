f3430035dc8ee0fa8f50ce284b270552cafe1af1
Sanket Banait <banaitsanket4@gmail.com>
2026-09-17 20:45:48 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (91.82%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -31,13 +31,16 @@ class Solution {
         if (root == null || root.val != head.val) {
             return false;
         }
-        return check(head.next, root.left) || check(head.next, root.right);
+        return check(head.next, root.left) 
+                || check(head.next, root.right);
     }
 
     public boolean isSubPath(ListNode head, TreeNode root) {
         if (root == null) {
             return false;
         }
-        return check(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+        return check(head, root) || 
+                isSubPath(head, root.left) || 
+                isSubPath(head, root.right);
     }
 }
