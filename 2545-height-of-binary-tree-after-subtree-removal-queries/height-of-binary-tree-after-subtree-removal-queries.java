e00d21c2a592275b8e5eca30cc54c01dbaecec8d
Sanket Banait <banaitsanket4@gmail.com>
2026-09-19 11:36:14 +0530

    Time: 19 ms (51.05%) | Memory: 275 MB (5.27%) - LeetSync

--- 2545-height-of-binary-tree-after-subtree-removal-queries/height-of-binary-tree-after-subtree-removal-queries.java
+++ 2545-height-of-binary-tree-after-subtree-removal-queries/height-of-binary-tree-after-subtree-removal-queries.java
@@ -19,14 +19,16 @@ class Solution {
     private int[] levelMaxHt = new int[100001];
     private int[] levelSecondMaxHt = new int[100001];
 
-    // Method to find the height of each node and populate the max heights at each level
+    // Method to find the height of each node 
+    // and populate the max heights at each level
     private int findHeight(TreeNode root, int l) {
         if (root == null) {
             return 0;
         }
 
         level[root.val] = l;
-        height[root.val] = Math.max(findHeight(root.left, l + 1), findHeight(root.right, l + 1)) + 1;
+        height[root.val] = Math.max(findHeight(root.left, l + 1), 
+                                findHeight(root.right, l + 1)) + 1;
 
         if (levelMaxHt[l] < height[root.val]) {
             levelSecondMaxHt[l] = levelMaxHt[l];
@@ -48,7 +50,9 @@ class Solution {
             int L = level[node]; // Node's level
 
             // Calculate result for this node
-            int tempResult = L + (levelMaxHt[L] == height[node] ? levelSecondMaxHt[L] : levelMaxHt[L]) - 1;
+            int tempResult = L + 
+                                (levelMaxHt[L] == height[node] ? levelSecondMaxHt[L] 
+                                : levelMaxHt[L]) - 1;
             result[i] = tempResult;
         }
 
