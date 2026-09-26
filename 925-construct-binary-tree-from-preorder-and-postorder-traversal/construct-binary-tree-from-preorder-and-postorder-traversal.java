4854899d72bbb4c4229103dfcf258e4f5f17fba0
Sanket Banait <banaitsanket4@gmail.com>
2026-09-26 10:08:11 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (81.94%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -15,7 +15,8 @@
  */
 class Solution {
 
-    public TreeNode helper(int[] preorder, int[] postorder,int presi,int preei,int postsi,int postei) {
+    public TreeNode helper(int[] preorder, int[] postorder,
+                            int presi,int preei,int postsi,int postei) {
         if(presi > preei){
             return null;
         }
@@ -25,20 +26,25 @@ class Solution {
             return root;
         }
         int idx = postsi;
-        while(presi+1 < preorder.length && postorder[idx] != preorder[presi+1]){
+        while(presi+1 < preorder.length 
+                && postorder[idx] != preorder[presi+1]){
             idx++;
         }
 
         int countOfLeftSubtreeNodes = idx - postsi + 1;
 
-        root.left = helper(preorder,postorder,presi+1,presi+countOfLeftSubtreeNodes,postsi,idx);
-        root.right = helper(preorder,postorder,presi+countOfLeftSubtreeNodes+1,preei,idx+1,postei-1);
+        root.left = helper(preorder,postorder,presi+1,
+                            presi+countOfLeftSubtreeNodes,postsi,idx);
+        root.right = helper(preorder,postorder,
+                            presi+countOfLeftSubtreeNodes+1,preei,idx+1,postei-1);
 
         return root;
     }
 
-    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
-        TreeNode root = helper(preorder,postorder,0,preorder.length-1,0,postorder.length-1);
+    public TreeNode constructFromPrePost(int[] preorder, 
+                                    int[] postorder) {
+        TreeNode root = helper(preorder,postorder,0,preorder.length-1,
+                                0,postorder.length-1);
         return root;
     }
 }
